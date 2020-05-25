0ae5beacde22ee926db81f3cdbaa9d66a4c0ecea
Petr Kuderov <pkuderov@gmail.com>
2020-05-25 15:07:35 +0300

    refactor: fix imports in mdp agent test

--- htm_rl/htm_rl/run_mdp_test.py
+++ htm_rl/htm_rl/run_mdp_test.py
@@ -1,17 +1,14 @@
 import numpy as np
 
-from agent import Agent
-from planner import Planner
-from representations.int_sdr_encoder import IntSdrEncoder
-from representations.sar import Sar, str_from_sar_superposition
-from representations.sar_sdr_encoder import SarSdrEncoder, SarSdrEncodersNT
-from representations.sar_value import decode_sar_value
-from temporal_memory import TemporalMemory
-# from tests import test_all
+from htm_rl.mdp_agent.agent import Agent
+from htm_rl.mdp_agent.planner import Planner
+from htm_rl.mdp_agent.sar import Sar
+from htm_rl.mdp_agent.sar_sdr_encoder import SarSdrEncodersNT, SarSdrEncoder
+from htm_rl.mdp_agent.sar_value import decode_sar_value
+from htm_rl.representations.int_sdr_encoder import IntSdrEncoder
+from htm_rl.representations.temporal_memory import TemporalMemory
 
 if __name__ == '__main__':
-    # test_all()
-
     sar_encoders = SarSdrEncodersNT(
         s=IntSdrEncoder('state', 6, 10, 8),
         a=IntSdrEncoder('action', 3, 5, 4),
@@ -21,7 +18,7 @@ if __name__ == '__main__':
 
     tm = TemporalMemory(
         n_columns=encoder.total_bits,
-        cells_per_column=1,
+        cells_per_column=2,
         activation_threshold=16, learning_threshold=12,
         initial_permanence=.5, connected_permanence=.5
     )
@@ -30,8 +27,8 @@ if __name__ == '__main__':
     sar_value_sequences = [
         [10, 120, 210, 301],
         [20, 410, 510, 301],
-        # [10, 120, 220, 410, 510, 301],
-        # [20, 410]
+        [10, 120, 220, 410, 510, 301],
+        [20, 410]
     ]
     train_samples = [
         [encoder.encode_sparse(x) for x in decode_sar_value(sequence)]
@@ -45,12 +42,10 @@ if __name__ == '__main__':
 
     initial_sar = Sar(0, -1, 0)
     initial_proximal_input = encoder.encode_sparse(initial_sar)
-    print(str_from_sar_superposition(encoder.decode_sparse(initial_proximal_input)))
     # start_value = train_samples[0][0]
 
     # agent.predict_cycle(initial_proximal_input, 3, print_enabled=True, reset_enabled=True)
     # agent.plan_to_value(start_value, 6, print_enabled=True, reset_enabled=True)
 
-    print(agent.tm.n_columns)
     planner = Planner(agent, 6, print_enabled=True)
     planner.plan_actions(initial_sar)
