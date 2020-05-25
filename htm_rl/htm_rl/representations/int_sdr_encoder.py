c01e10c2be15efa1ba6d0fb7d8dcc3d627f4bfa4
Petr Kuderov <pkuderov@gmail.com>
2020-05-26 01:34:06 +0300

    refactor: int sdr encoding

--- htm_rl/htm_rl/representations/int_sdr_encoder.py
+++ htm_rl/htm_rl/representations/int_sdr_encoder.py
@@ -1,88 +1,170 @@
-from typing import List, Tuple, Iterable
+from dataclasses import dataclass, astuple
+from typing import Tuple, Iterable
 
-import numpy as np
-
+from htm_rl.mdp_agent.sar import Superposition
 from htm_rl.representations.sdr import SparseSdr, DenseSdr
 from htm_rl.utils import isnone, sparse_to_dense
+
+
+@dataclass(frozen=True)
+class BitRange:
+    __slots__ = ['l', 'r']
+    l: int
+    r: int
+
+    # is needed for tuple unpacking
+    def __iter__(self):
+        yield from astuple(self)
+
+    @staticmethod
+    def unfold(buckets: Iterable['BitRange']) -> SparseSdr:
+        return [
+            i
+            for bucket in buckets
+            for i in range(bucket.l, bucket.r)
+        ]
 
 
 class IntSdrEncoder:
     ALL = -1
 
     name: str
-    n_values: int
-    value_bits: int
+    _n_values: int
+    _value_bits: int
     total_bits: int
-    activation_threshold: int
+    _activation_threshold: int
 
-    def __init__(self, name: str, n_vals: int, value_bits: int, activation_threshold: int = None):
+    def __init__(self, name: str, n_values: int, value_bits: int, activation_threshold: int = None):
+        self._value_bits = value_bits
+        self._n_values = n_values
+        self._activation_threshold = isnone(activation_threshold, self._value_bits)
+
         self.name = name
-        self.value_bits = value_bits
-        self.n_values = n_vals
-        self.total_bits = n_vals * value_bits
-        self.activation_threshold = isnone(activation_threshold, self.value_bits)
+        self.total_bits = n_values * value_bits
 
-    def encode_sparse_with_shift(self, x: int, shift: int) -> Iterable[int]:
-        ind_from, ind_to = self._encoding_bits_range(x)
-        return range(ind_from + shift, ind_to + shift)
+    def encode(self, x: int, shift: int) -> Iterable[BitRange]:
+        l, r = self._bit_bucket_range(x)
+        yield BitRange(l + shift, r + shift)
 
-    def encode_sparse(self, x: int) -> SparseSdr:
-        ind_from, ind_to = self._encoding_bits_range(x)
-        return list(range(ind_from, ind_to))
-
-    def _encoding_bits_range(self, x: int) -> Tuple[int, int]:
-        assert x is None or x == self.ALL or 0 <= x < self.n_values, \
-            f'Value must be in [0, {self.n_values}] or {self.ALL} or None; got {x}'
-
-        if x is None:
-            return 0, 0
-        if x == self.ALL:
-            return 0, self.total_bits
-
-        ind_from = x * self.value_bits
-        ind_to = ind_from + self.value_bits
-        return ind_from, ind_to
-
-    def decode_sparse(self, indices: SparseSdr) -> List[int]:
-        n_activations = [0]*self.n_values
+    def decode(self, indices: Iterable[int]) -> Superposition:
+        n_activations = [0] * self._n_values
         for x in indices:
-            value = x // self.value_bits
+            value = x // self._value_bits
             n_activations[value] += 1
 
         decoded_values = [
             value
             for value, n_act in enumerate(n_activations)
-            if n_act >= self.activation_threshold
+            if n_act >= self._activation_threshold
         ]
         return decoded_values
 
-    def str_from_sparse(self, indices: SparseSdr):
-        return self._str_from_dense(sparse_to_dense(indices, self.total_bits))
+    def format(self, indices: SparseSdr) -> str:
+        dense_sdr = sparse_to_dense(indices, self.total_bits)
+        return self._str_from_dense(dense_sdr)
 
-    def _encode_dense(self, x: int, arr: DenseSdr = None) -> DenseSdr:
-        ind_from, ind_to = self._encoding_bits_range(x)
-        if arr is None:
-            arr = np.zeros(self.total_bits, dtype=np.int8)
-        arr[ind_from:ind_to] = 1
-        return arr
+    def _bit_bucket_range(self, x: int) -> BitRange:
+        self._assert_acceptable_values(x)
 
-    def _decode_dense(self, arr: DenseSdr) -> List[int]:
-        decoded_values = []
-        for i in range(0, self.total_bits, self.value_bits):
-            n_activations = np.count_nonzero(arr[i: i + self.value_bits])
-            if n_activations >= self.activation_threshold:
-                decoded_values.append(i // self.value_bits)
+        if x is None:
+            return BitRange(0, 0)
+        if x == self.ALL:
+            return BitRange(0, self.total_bits)
 
-        return decoded_values
+        l = x * self._value_bits
+        r = l + self._value_bits
+        return BitRange(l, r)
 
-    def _str_from_dense(self, arr: DenseSdr):
-        assert arr.ndim == 1, f'Expected 1-dim array; got {arr.ndim}'
-        bit_packs_by_value = [
-            ''.join(map(str, arr[i: i + self.value_bits]))
-            for i in range(0, self.total_bits, self.value_bits)
-        ]
-        return ' '.join(bit_packs_by_value)
+    def _str_from_dense(self, arr: DenseSdr) -> str:
+        bit_buckets = (
+            arr[i: i + self._value_bits]
+            for i in range(0, self.total_bits, self._value_bits)
+        )
+        return ' '.join(
+            ''.join(map(str, bucket))
+            for bucket in bit_buckets
+        )
+
+    def _assert_acceptable_values(self, x: int):
+        assert x is None or x == self.ALL or 0 <= x < self._n_values, \
+            f'Value must be in [0, {self._n_values}] or {self.ALL} or None; got {x}'
 
     def __str__(self):
-        name, n_values, value_bits = self.name, self.n_values, self.value_bits
+        name, n_values, value_bits = self.name, self._n_values, self._value_bits
         return f'({name}: v{n_values} x b{value_bits})'
+
+
+# class IntSdrEncoderBackup:
+#     ALL = -1
+#
+#     name: str
+#     n_values: int
+#     value_bits: int
+#     total_bits: int
+#     activation_threshold: int
+#
+#     def __init__(self, name: str, n_vals: int, value_bits: int, activation_threshold: int = None):
+#         self.name = name
+#         self.value_bits = value_bits
+#         self.n_values = n_vals
+#         self.total_bits = n_vals * value_bits
+#         self.activation_threshold = isnone(activation_threshold, self.value_bits)
+#
+#     def encode_chained(self, x: int, shift: int) -> Iterable[int]:
+#         l, r = self._bit_bucket_range(x)
+#         return range(l + shift, r + shift)
+#
+#     def decode_chained(self, indices: Iterable[int], shift: int) -> Superposition[int]:
+#         n_activations = [0] * self.n_values
+#         for x in indices:
+#             value = (x - shift) // self.value_bits
+#             n_activations[value] += 1
+#
+#         decoded_values = [
+#             value
+#             for value, n_act in enumerate(n_activations)
+#             if n_act >= self.activation_threshold
+#         ]
+#         return decoded_values
+#
+#     def encode(self, x: int) -> SparseSdr:
+#         return list(self.encode_chained(x, 0))
+#
+#     def decode(self, indices: SparseSdr) -> Superposition[int]:
+#         return self.decode_chained(indices, 0)
+#
+#     def to_str(self, indices: SparseSdr):
+#         dense_sdr = sparse_to_dense(indices, self.total_bits)
+#         return self._str_from_dense(dense_sdr)
+#
+#     def __str__(self):
+#         name, n_values, value_bits = self.name, self.n_values, self.value_bits
+#         return f'({name}: v{n_values} x b{value_bits})'
+#
+#     def _bit_bucket_range(self, x: int) -> Tuple[int, int]:
+#         self._assert_acceptable_values(x)
+#
+#         if x is None:
+#             return 0, 0
+#         if x == self.ALL:
+#             return 0, self.total_bits
+#
+#         ind_from = x * self.value_bits
+#         ind_to = ind_from + self.value_bits
+#         return ind_from, ind_to
+#
+#     def _str_from_dense(self, arr: DenseSdr):
+#         assert arr.ndim == 1, f'Expected 1-dim array; got {arr.ndim}'
+#         bit_buckets = (
+#             arr[i: i + self.value_bits]
+#             for i in range(0, self.total_bits, self.value_bits)
+#         )
+#         return ' '.join(
+#             ''.join(map(str, bucket))
+#             for bucket in bit_buckets
+#         )
+#
+#     def _assert_acceptable_values(self, x: int):
+#         assert x is None or x == self.ALL or 0 <= x < self.n_values, \
+#             f'Value must be in [0, {self.n_values}] or {self.ALL} or None; got {x}'
+#
