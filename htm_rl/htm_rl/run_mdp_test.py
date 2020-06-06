709ff352afaa99084ab5789994a7583cdf8effb7
Petr Kuderov <pkuderov@gmail.com>
2020-06-07 00:34:47 +0300

    added mdp env to mdp test

--- htm_rl/htm_rl/run_mdp_test.py
+++ htm_rl/htm_rl/run_mdp_test.py
@@ -1,52 +1,123 @@
+import random
+
+import matplotlib.pyplot as plt
 import numpy as np
 
 from htm_rl.agent import Agent
-from htm_rl.mdp_agent.sar import SarSuperpositionFormatter, Sar
+from htm_rl.envs.mdp import generate_gridworld_mdp
+from htm_rl.mdp_agent.sar import Sar, SarSuperpositionFormatter
 from htm_rl.mdp_agent.sar_sdr_encoder import SarSdrEncoder
-from htm_rl.mdp_agent.sar_value import decode_sar_value
 from htm_rl.planner import Planner
 from htm_rl.representations.int_sdr_encoder import IntSdrEncoder
 from htm_rl.representations.temporal_memory import TemporalMemory
 
 
+def render_env(env, render):
+    if not render:
+        return
+    env.render()
+
+
+def print_debug_sar(sar, encoder, sar_formatter):
+    indices = encoder.encode(sar)
+    print(encoder.format(indices))
+    sar_superposition = encoder.decode(indices)
+    print(sar_formatter.format(sar_superposition))
+
+
+def train_for(n_steps, observation, reward, print_enabled):
+    reward_reached = 0
+    done = False
+    for _ in range(n_steps):
+        action = np.random.choice(2)
+
+        render_env(env, render)
+        sar = Sar(observation, action, reward)
+        proximal_input = encoder.encode(sar)
+        agent.train_one_step(proximal_input, print_enabled)
+
+        next_observation, reward, done, info = env.step(action)
+        observation = next_observation
+        if reward > 0:
+            reward = 1
+            reward_reached += 1
+
+        if done:
+            action = 0 # any next action
+            render_env(env, render)
+            sar = Sar(observation, action, reward)
+            proximal_input = encoder.encode(sar)
+            agent.train_one_step(proximal_input, print_enabled)
+            break
+
+    if done and reward != 1:
+        agent.tm.reset()
+
+    return reward_reached
+
+
 if __name__ == '__main__':
+    random.seed(1337)
+    np.random.seed(1337)
+
+    env = generate_gridworld_mdp(
+        initial_state=(0, 0),   # c0 >
+        cell_transitions=[
+            (0, 0, 1),      # c0 > c1
+        ],
+    )
+
+    observation, reward, done = env.reset(), 0, False
+    n = env.n_states
+    print(f'States: {n}')
+
+    bs, ba, br = 6, 10, 10
     encoder = SarSdrEncoder((
-        IntSdrEncoder('state', 6, 10, 8),
-        IntSdrEncoder('action', 3, 5, 4),
-        IntSdrEncoder('reward', 2, 5, 4),
+        IntSdrEncoder('state', n, bs, bs-1),
+        IntSdrEncoder('action', 3, ba, ba-1),
+        IntSdrEncoder('reward', 2, br, br-1)
     ))
+    sar_formatter = SarSuperpositionFormatter
 
     activation_threshold = encoder.activation_threshold
-    learning_threshold = int(0.66*activation_threshold)
+    learning_threshold = int(0.9*activation_threshold)
+    print('Bits, act, learn:', encoder.total_bits, activation_threshold, learning_threshold)
+
     tm = TemporalMemory(
         n_columns=encoder.total_bits,
-        cells_per_column=2,
+        cells_per_column=8,
         activation_threshold=activation_threshold, learning_threshold=learning_threshold,
-        initial_permanence=.5, connected_permanence=.5
+        initial_permanence=.5, connected_permanence=.25,
+        maxNewSynapseCount=int(1.1 * encoder.value_bits),
+        predictedSegmentDecrement=.0001,
+        permanenceIncrement=.1,
+        permanenceDecrement=.1,
     )
-    agent = Agent(tm, encoder, SarSuperpositionFormatter.format)
+    agent = Agent(tm, encoder, sar_formatter.format)
 
-    sar_value_sequences = [
-        [10, 120, 210, 301],
-        [20, 410, 510, 301],
-        [10, 120, 220, 410, 510, 301],
-        [20, 410]
-    ]
-    train_samples = [
-        [encoder.encode(x) for x in decode_sar_value(sequence)]
-        for sequence in sar_value_sequences
-    ]
-    n_train_samples = len(train_samples)
+    render, pause = False, .1
+    reward_reached = 0
+    for _ in range(50):
+        reward_reached += train_for(40, observation, reward)
+        tm.reset()
+        observation = env.reset()
+        reward = 0
+        a_ind = 0
+    print(f'Reward reached: {reward_reached}')
 
-    for _ in range(n_train_samples * 40):
-        train_sample = train_samples[np.random.choice(n_train_samples)]
-        agent.train_cycle(train_sample, print_enabled=False, reset_enabled=True)
+    # train_for(10, observation, reward, 10, True)
 
-    initial_sar = Sar(0, -1, 0)
+    initial_sar = Sar(observation, 2, 0)
     initial_proximal_input = encoder.encode(initial_sar)
-    # start_value = train_samples[0][0]
+    # agent.predict_cycle(initial_proximal_input, 20, True)
 
-    agent.predict_cycle(initial_proximal_input, 3, print_enabled=True, reset_enabled=True)
-
-    planner = Planner(agent, 6, print_enabled=True)
+    planner = Planner(agent, 5, print_enabled=True)
     planner.plan_actions(initial_sar)
+
+    agent.tm.printParameters()
+
+    plt.figure(figsize=(12, 6))
+    anomalies = agent.anomalies
+    xs = np.arange(len(anomalies))
+    plt.plot(xs, anomalies)
+    plt.show()
