d4b3806ee60105f39e0a4e91432a69f0095d09a9
Petr Kuderov <pkuderov@gmail.com>
2020-07-24 15:41:33 +0300

    add conclusions to the report

--- htm_rl/setup.py
+++ htm_rl/setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='htm_rl',
     version='0.1',
-    description='TODO',
-    url='TODO',
-    author='TODO',
-    author_email='TODO',
+    description='RL agent based on HTM framework',
+    url='https://github.com/cog-isa/htm-rl/',
+    author='Petr Kuderov',
+    author_email='pkuderov@gmail.com',
     license='MIT',
     packages=find_packages(),
 )
