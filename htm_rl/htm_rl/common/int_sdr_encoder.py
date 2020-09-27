9e0543ffc81fa9be09e87b92605e9a023afa8d1a
gsys <zhe>
2020-09-27 20:04:12 +0300

    Semantic int encoder is added.

--- htm_rl/htm_rl/common/int_sdr_encoder.py
+++ htm_rl/htm_rl/common/int_sdr_encoder.py
@@ -175,6 +175,89 @@ class IntSdrEncoder(Generic[ValueEncoder]):
         return f'({name}: v{n_values} x b{value_bits})'
 
 
+class IntSemanticSdrEncoder(IntSdrEncoder):
+    """
+    Encodes integer values from range [0, `n_values`) as SDR w/ `total_bits` bits.
+    Every single value x from the range is encoded as `value_bits` contiguous 1 bits called
+        a bucket. All bits outside corresponding bucket are set to zero.
+    To achieve semantic similarity use step parameter. Step is a half amount of bits that make up difference
+    of buckets of two adjacent integer values.
+
+
+    Example:
+        An encoder(n_values = 3, value_bits = 4, step=2) encodes values as:
+            0: 11110000
+            1: 00111100
+            2: 00001111
+    """
+
+    step: int
+    overlap: float
+
+    def __init__(self, name: str, n_values: int, value_bits: int, step: int, activation_threshold: int,
+                 default_format: str = 'full'):
+        super(IntSemanticSdrEncoder, self).__init__(name, n_values, value_bits, activation_threshold,
+                                                    default_format)
+        """
+        Initializes encoder.
+
+        :param name: a name for values encoded
+        :param n_values: defines a range [0, n_values) of values that can be encoded
+        :param value_bits: a number of bits in a bucket used to encode single value
+        :param step: a half number of bits that make up difference of buckets 
+            of two adjacent integer values.
+        :param activation_threshold: a number of 1 bits in a bucket, which are enough
+            to activate corresponding value, i.e. this value will be decoded.
+        """
+
+        self.step = step
+        self.total_bits = self.step * (self.n_values - 1) + self.value_bits
+        self.overlap = (self.value_bits - self.step) / self.value_bits
+
+    def value_activations(self, sparse_sdr: SparseSdr) -> Iterable[int]:
+        """
+           Counts number of activations in each value's bucket of bits in a given sparse SDR.
+           :param sparse_sdr: sparse SDR
+           :return: numpy array of activations - i-th elem is an activation of value i.
+        """
+
+        value_activations = np.zeros(self.n_values, dtype=np.int64)
+        for active_bit_index in sparse_sdr:
+            max_value = active_bit_index // self.step
+            min_value = (active_bit_index - self.value_bits + 1) // self.step
+
+            value_activations[min_value: max_value+1] += 1
+
+        return value_activations
+
+    def _bit_bucket_range(self, x: int) -> BitRange:
+        """Gets BitRange [l, r) of the bit bucket corresponding to the given value `x`."""
+        self._assert_acceptable_values(x)
+
+        if x is None:
+            return BitRange(0, 0)
+        if x == self.ALL:
+            return BitRange(0, self.total_bits)
+
+        left = x * self.step
+        right = left + self.value_bits
+        return BitRange(left, right)
+
+    def format(self, sparse_sdr: SparseSdr, format_: str = None) -> str:
+        """
+        Formats sparse SDR to string with one of the supported formats.
+        """
+        format_ = isnone(format_, self.default_format)
+        supported_formats = {
+            'full': IntSemanticSdrFormatter
+        }
+        return supported_formats[format_].format(sparse_sdr, self)
+
+    def __str__(self):
+        name, n_values, value_bits = self.name, self.n_values, self.value_bits
+        return f'({name}: v{n_values} x b{value_bits} step: {self.step})'
+
+
 class SequenceSdrEncoder(Generic[SequenceEncoder]):
     """
         Encodes sequence as SDR. Sequence should consist of numbers of types that have encoders.
@@ -300,3 +383,25 @@ class IntSdrShortFormatter:
         ]
 
         return ' '.join(cls.format_chars[x] for x in encoded_sdr_for_printing)
+
+
+class IntSemanticSdrFormatter:
+    """
+        Formats SDR as a dense array:
+            000011110000
+    """
+
+    @staticmethod
+    def format(sparse_sdr: SparseSdr, encoder: IntSdrEncoder) -> str:
+        """
+        :param sparse_sdr: SDR to print into string
+        :param encoder: encoder for this particular kind of SDRs
+        """
+        dense_sdr = sparse_to_dense(sparse_sdr, encoder.total_bits)
+        value_bit_buckets = (
+            dense_sdr[i: i + encoder.value_bits]
+            for i in range(0, encoder.total_bits, encoder.value_bits)
+        )
+        return ''.join(
+            ''.join(map(str, value_bits)) for value_bits in value_bit_buckets
+        )
