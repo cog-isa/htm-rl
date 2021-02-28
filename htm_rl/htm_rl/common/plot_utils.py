72ff81ce6a7b81eb997667ca82aa446504e741e8
Petr Kuderov <pkuderov@gmail.com>
2021-03-01 01:37:59 +0300

    typos

--- htm_rl/htm_rl/common/plot_utils.py
+++ htm_rl/htm_rl/common/plot_utils.py
@@ -2,14 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_grid_image(img: np.ndarray, uncenter = False, grid = False):
+def plot_grid_image(img: np.ndarray, uncenter=False, grid=False):
     plt.imshow(img)
     if uncenter:
         shape = img.shape
-        xticks = np.arange(shape[1] + 1)
-        yticks = np.arange(shape[0], -1, -1)
-        plt.xticks(xticks - .5, xticks)
-        plt.yticks(yticks - .5, yticks)
+        x_ticks = np.arange(shape[1] + 1)
+        y_ticks = np.arange(shape[0], -1, -1)
+        plt.xticks(x_ticks - .5, x_ticks)
+        plt.yticks(y_ticks - .5, y_ticks)
+
     ax = plt.gca()
     ax.xaxis.tick_top()
     if grid:
