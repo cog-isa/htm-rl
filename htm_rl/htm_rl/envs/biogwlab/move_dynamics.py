b37afbf5e3c5b336549020806773304e5f19e5b3
Petr Kuderov <pkuderov@gmail.com>
2021-03-03 14:08:58 +0300

    comment move dynamics internals

--- htm_rl/htm_rl/envs/biogwlab/move_dynamics.py
+++ htm_rl/htm_rl/envs/biogwlab/move_dynamics.py
@@ -1,7 +1,6 @@
 from typing import Tuple
 
 import numpy as np
-from numpy.core._multiarray_umath import ndarray
 
 from htm_rl.common.utils import clip
 
@@ -15,13 +14,16 @@ TURN_DIRECTIONS = {'right': 1, 'left': -1}
 
 class MoveDynamics:
     @staticmethod
-    def turn(view_direction, turn_direction):
+    def turn(view_direction: int, turn_direction: int):
         new_direction = (view_direction + turn_direction + 4) % 4
         return new_direction
 
     @classmethod
-    def try_move(cls, position, direction, shape, obstacle_mask):
-        new_position = cls.move(position, direction)
+    def try_move(
+            cls, position: Tuple[int, int], move_direction: Tuple[int, int],
+            shape: Tuple[int, int], obstacle_mask: np.ndarray
+    ):
+        new_position = cls.move(position, move_direction)
         new_position = cls.clip2d(new_position, shape)
         success = cls.is_move_successful(position, new_position, obstacle_mask)
         if not success:
@@ -29,18 +31,22 @@ class MoveDynamics:
         return new_position, success
 
     @staticmethod
-    def is_move_successful(position, new_position, obstacle_mask):
+    def is_move_successful(
+            position: Tuple[int, int], new_position: Tuple[int, int],
+            obstacle_mask: np.ndarray
+    ):
+        # changed position but not into the wall
         return new_position != position and not obstacle_mask[new_position]
 
     @staticmethod
-    def move(position, direction):
+    def move(position: Tuple[int, int], direction: Tuple[int, int]):
         i, j = position
         i += direction[0]
         j += direction[1]
         return i, j
 
     @staticmethod
-    def clip2d(position, shape):
+    def clip2d(position: Tuple[int, int], shape: Tuple[int, int]):
         i, j = position
         i = clip(i, shape[0])
         j = clip(j, shape[1])
