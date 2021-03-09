8725f43e36ba8a9a97db5f8fd4c0621f42e4cca1
Petr Kuderov <pkuderov@gmail.com>
2021-03-09 13:30:21 +0300

    refactor add modules func in Env

--- htm_rl/htm_rl/envs/biogwlab/environment.py
+++ htm_rl/htm_rl/envs/biogwlab/environment.py
@@ -30,31 +30,20 @@ class BioGwLabEnvironment:
         state.set_action_costs(**action_costs)
         state.set_regenerator(**regenerator)
 
-        BioGwLabEnvironment.add_module(state, modules, 'areas')
-        BioGwLabEnvironment.add_module(state, modules, 'obstacles')
-        BioGwLabEnvironment.add_module(state, modules, 'food')
+        add_module(state, modules, 'areas')
+        add_module(state, modules, 'obstacles')
+        add_module(state, modules, 'food')
         state.add_agent()
 
         # print(state.modules)
         # print(state.handlers)
         state.reset()
 
-        BioGwLabEnvironment.add_module(state, modules, 'rendering')
+        add_module(state, modules, 'rendering')
 
         self.state = state
         sdr = state.render()
         # print(sdr)
-
-    @staticmethod
-    def add_module(env, modules, name):
-        config = modules[name]
-        module_type = name
-        if module_type not in registrar:
-            module_type = config['_type_']
-            config.pop('_type_')
-
-        module = registrar[module_type](env=env, **config)
-        env.add_module(name, module)
 
     def observe(self):
         return self.state.observe()
@@ -74,3 +63,14 @@ class BioGwLabEnvironment:
     @property
     def output_sdr_size(self):
         return self.state.output_sdr_size
+
+
+def add_module(env, modules, name):
+    config = modules[name]
+    module_type = name
+    if module_type not in registrar:
+        module_type = config['_type_']
+        config.pop('_type_')
+
+    module = registrar[module_type](env=env, **config)
+    env.add_module(name, module)
