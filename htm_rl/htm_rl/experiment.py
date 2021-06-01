e01f658f3ae2bfa5ab1a58b7fdb2b5be07c5e41e
Petr Kuderov <pkuderov@gmail.com>
2021-06-01 19:11:46 +0300

    support agent-level recorders with experiment-level recorders

--- htm_rl/htm_rl/experiment.py
+++ htm_rl/htm_rl/experiment.py
@@ -1,14 +1,15 @@
 from pathlib import Path
+from typing import Optional, Any
 
 import numpy as np
 from tqdm import trange
 
 from htm_rl.agents.agent import Agent
 from htm_rl.agents.rnd.agent import RndAgent
-from htm_rl.agents.svpn.agent import SvpnAgent
+from htm_rl.agents.svpn.agent import SvpnAgent, ValueRecorder, TDErrorRecorder, AnomalyRecorder
 from htm_rl.agents.ucb.agent import UcbAgent
 from htm_rl.common.plot_utils import store_environment_map, plot_grid_images
-from htm_rl.common.utils import timed
+from htm_rl.common.utils import timed, ensure_list
 from htm_rl.config import FileConfig
 from htm_rl.envs.biogwlab.env import BioGwLabEnvironment
 from htm_rl.envs.biogwlab.wrappers.recorders import AgentPositionRecorder
@@ -39,10 +40,24 @@ class Experiment:
         if self.print['heatmaps'] is not None:
             env = AgentPositionRecorder(env)
             heatmap_recorders = [
-                HeatmapRecorder(freq, config, env_shape)
+                HeatmapRecorder(config, freq, env_shape)
                 for freq in self.print['heatmaps']
             ]
             handlers.extend(heatmap_recorders)
+
+        if self.print['debug'] is not None:
+            env = AgentPositionRecorder(env)
+            agent = ValueRecorder(agent)
+            agent = TDErrorRecorder(agent)
+            agent = AnomalyRecorder(agent)
+            for freq in self.print['debug']:
+                aggregator = AggregateRecorder(config, freq)
+                handlers.extend([
+                    MapRecorder(config, freq, aggregator),
+                    HeatmapRecorder(config, freq, env_shape, aggregator),
+                    ValueMapRecorder(config, freq, env_shape, aggregator),
+                    aggregator
+                ])
 
         train_stats = RunStats()
         wandb_run = self.init_wandb_run(agent, config) if self.wandb['enabled'] else None
@@ -127,18 +142,67 @@ class Experiment:
             handler.handle_step(env, agent, step, )
 
 
-class HeatmapRecorder:
+class BaseRecorder:
     config: FileConfig
+    save_dir: Path
+    aggregator: Optional[Any]
+
+    def __init__(self, config: FileConfig, aggregator=None):
+        self.config = config
+        self.save_dir = self.config['results_dir']
+        self.aggregator = aggregator
+
+    def handle_step(self, env: Env, agent: Agent, step: int):
+        raise NotImplementedError
+
+    def handle_episode(self, env: Env, agent: Agent, episode: int):
+        raise NotImplementedError
+
+
+class AggregateRecorder(BaseRecorder):
+    images: list[np.ndarray]
+    titles: list[str]
+    name_str: str
+
+    def __init__(self, config: FileConfig, frequency):
+        super().__init__(config)
+
+        self.name_str = f'debug_{config["agent"]}_{config["env_seed"]}_{config["agent_seed"]}_{frequency}'
+        self.images = []
+        self.titles = []
+
+    def handle_step(self, env: Env, agent: Agent, step: int):
+        self._flush_images(step + 1)
+
+    def handle_episode(self, env: Env, agent: Agent, episode: int):
+        self._flush_images(episode + 1)
+
+    def handle_img(self, image: np.ndarray, title: str = None):
+        self.images.append(image.copy())
+        if title is not None:
+            self.titles.append(title)
+
+    def _flush_images(self, ind: int):
+        if not self.images:
+            return
+        save_path = self.save_dir.joinpath(f'{self.name_str}_{ind}.svg')
+        plot_grid_images(
+            images=self.images, titles=self.titles,
+            show=False, save_path=save_path
+        )
+        self.images.clear()
+        self.titles.clear()
+
+
+class HeatmapRecorder(BaseRecorder):
     heatmap: np.ndarray
     frequency: int
     name_str: str
-    save_dir: Path
 
-    def __init__(self, frequency: int, config: FileConfig, shape):
-        self.config = config
+    def __init__(self, config: FileConfig, frequency: int, shape, aggregator=None):
+        super().__init__(config, aggregator)
         self.frequency = frequency
-        self.name_str = f'{config["agent"]}_{config["env_seed"]}_{config["agent_seed"]}_{frequency}'
-        self.save_dir = self.config['results_dir']
+        self.name_str = f'heatmap_{config["agent"]}_{config["env_seed"]}_{config["agent_seed"]}_{frequency}'
         self.heatmap = np.zeros(shape, dtype=np.float)
 
     def handle_step(self, env: Env, agent: Agent, step: int):
@@ -151,13 +215,92 @@ class HeatmapRecorder:
 
     def _flush_heatmap(self, episode):
         plot_title = f'{self.name_str}_{episode}'
-        save_path = self.save_dir.joinpath(f'heatmap_{plot_title}.svg')
-        plot_grid_images(
-            images=self.heatmap, titles=plot_title,
-            show=False, save_path=save_path
-        )
+        if self.aggregator is not None:
+            self.aggregator.handle_img(self.heatmap, plot_title)
+        else:
+            save_path = self.save_dir.joinpath(f'{plot_title}.svg')
+            plot_grid_images(
+                images=self.heatmap, titles=plot_title,
+                show=False, save_path=save_path
+            )
         # clear heatmap
         self.heatmap.fill(0.)
+
+
+class ValueMapRecorder(BaseRecorder):
+    fill_value: float = -1.
+
+    value_map: np.ndarray
+    frequency: int
+    name_str: str
+    last_value: Optional[float]
+
+    def __init__(self, config: FileConfig, frequency: int, shape, aggregator=None):
+        super().__init__(config, aggregator)
+        self.frequency = frequency
+        self.name_str = f'valuemap_{config["agent"]}_{config["env_seed"]}_{config["agent_seed"]}_{frequency}'
+        self.value_map = np.full(shape, self.fill_value, dtype=np.float)
+        self.last_value = None
+
+    def handle_step(self, env: Env, agent: Agent, step: int):
+        position: tuple[int, int] = env.get_info()['agent_position']
+        if self.last_value is not None:
+            self.value_map[position] = self.last_value
+
+        self.last_value = agent.get_info()['value']
+
+    def handle_episode(self, env: Env, agent: Agent, episode: int):
+        if (episode + 1) % self.frequency == 0:
+            self._flush_map(episode + 1)
+
+    def _flush_map(self, episode):
+        plot_title = f'{self.name_str}_{episode}'
+        if self.aggregator is not None:
+            self.aggregator.handle_img(self.value_map, plot_title)
+        else:
+            save_path = self.save_dir.joinpath(f'{plot_title}.svg')
+            plot_grid_images(
+                images=self.value_map, titles=plot_title,
+                show=False, save_path=save_path
+            )
+        # clear heatmap
+        self.value_map.fill(self.fill_value)
+
+
+class MapRecorder(BaseRecorder):
+    env_map: Optional[np.ndarray]
+    frequency: int
+    name_str: str
+    titles: list[str]
+
+    def __init__(self, config: FileConfig, frequency: int, aggregator=None):
+        super().__init__(config, aggregator)
+        self.frequency = frequency
+        self.name_str = f'map_{config["env"]}_{config["env_seed"]}'
+        self.titles = [
+            f'{config["env"]}, seed={config["env_seed"]}',
+            'agent observation'
+        ]
+        self.env_map = None
+
+    def handle_step(self, env: Env, agent: Agent, step: int):
+        if self.env_map is None:
+            self.env_map = ensure_list(env.callmethod('render_rgb'))
+
+    def handle_episode(self, env: Env, agent: Agent, episode: int):
+        if (episode + 1) % self.frequency == 0:
+            self._flush_map()
+
+    def _flush_map(self):
+        if self.env_map is None:
+            return
+
+        if self.aggregator is not None:
+            for i in range(len(self.env_map)):
+                self.aggregator.handle_img(self.env_map[i], self.titles[i])
+        else:
+            save_path = self.save_dir.joinpath(f'map_{self.name_str}.svg')
+            plot_grid_images(self.env_map, self.titles, show=False, save_path=save_path)
 
 
 class RunStats:
