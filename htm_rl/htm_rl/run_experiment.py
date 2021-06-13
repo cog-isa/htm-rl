ced53e14f41d244b9931ac63f59320a2b9e9603e
Petr Kuderov <pkuderov@gmail.com>
2021-06-13 22:01:51 +0300

    support unregistered run args to be parsed and add/overwrite config

--- htm_rl/htm_rl/run_experiment.py
+++ htm_rl/htm_rl/run_experiment.py
@@ -1,12 +1,12 @@
 import argparse
 import os
 from argparse import ArgumentParser
+from ast import literal_eval
 from itertools import product
 from pathlib import Path
+from pprint import pprint
 
 import numpy as np
-import tqdm
-from tqdm import trange
 
 from htm_rl.common.utils import ensure_list
 from htm_rl.config import FileConfig
@@ -17,12 +17,13 @@ class RunConfig(FileConfig):
     def __init__(self, path):
         super(RunConfig, self).__init__(path)
 
-    def run(self, agents_filter: list[str], envs_filter: list[str]):
+    def run(self, agents_filter: list[str], envs_filter: list[str], overwrites: list[str]):
         experiment = Experiment(**self)
         env_seeds = self['env_seeds']
         agent_seeds = self['agent_seeds']
         env_names = self.filter_by(self.read_subconfigs('envs', prefix='env'), envs_filter)
         agent_names = self.filter_by(self.read_subconfigs('agents', prefix='agent'), agents_filter)
+        self.add_overwrite_attributes(overwrites)
 
         experiment_setups = list(product(env_names, agent_names, env_seeds))
         for env_name, agent_name, env_seed in experiment_setups:
@@ -62,6 +63,40 @@ class RunConfig(FileConfig):
         self[key] = configs
         return self[key].keys()
 
+    def add_overwrite_attributes(self, overwrites: list[str]):
+        i = 0
+        while i < len(overwrites):
+            key = overwrites[i]
+            if not key.startswith('--'):
+                raise ValueError(
+                    f'Config attribute name started with `--` is expected, got `{key}`!'
+                )
+            key = key[2:]
+            value = overwrites[i+1]
+            if value.startswith('--'):
+                raise ValueError(
+                    f'Config attribute value is expected, got `{value}`, which looks like an attribute name!'
+                )
+            value = self.parse_str(value)
+            self[key] = value
+            i += 2
+
+    @staticmethod
+    def parse_str(val):
+        def boolify(s):
+            if s in ['True', 'true']:
+                return True
+            if s in ['False', 'false']:
+                return False
+            raise ValueError('Not Boolean Value!')
+
+        for caster in (boolify, int, float, literal_eval):
+            try:
+                return caster(val)
+            except ValueError:
+                pass
+        return val
+
     @staticmethod
     def filter_by(names, names_filter):
         if names_filter is None:
@@ -97,7 +132,7 @@ def register_arguments(parser: ArgumentParser):
 def main():
     parser = argparse.ArgumentParser()
     register_arguments(parser)
-    args = parser.parse_args()
+    args, overwrites = parser.parse_known_args()
 
     config_filename: str = args.config
     if not config_filename.startswith('expm_') or not config_filename.endswith('.yml'):
@@ -121,7 +156,7 @@ def main():
         os.environ['WANDB_MODE'] = 'dryrun'
         # os.environ['WANDB_SILENT'] = 'true'
 
-    config.run(args.agent, args.env)
+    config.run(args.agent, args.env, overwrites)
 
 
 if __name__ == '__main__':
