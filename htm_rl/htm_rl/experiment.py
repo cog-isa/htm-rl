2a1568387e304678ac3db80acbae34bc42af1632
Petr Kuderov <pkuderov@gmail.com>
2021-06-24 20:35:48 +0300

    refactor Experiment

--- htm_rl/htm_rl/experiment.py
+++ htm_rl/htm_rl/experiment.py
@@ -1,78 +1,112 @@
 import numpy as np
 from tqdm import trange
 
-from htm_rl.agents.agent import Agent, Wrapper
+from htm_rl.agents.agent import Agent
 from htm_rl.agents.rnd.agent import RndAgent
 from htm_rl.agents.svpn.agent import SvpnAgent
-from htm_rl.agents.svpn.debug import ValueProvider, TDErrorProvider, AnomalyProvider, DreamingLengthProvider
 from htm_rl.agents.ucb.agent import UcbAgent
 from htm_rl.common.debug import inject_debug_tools
-from htm_rl.common.utils import timed, wrap
-from htm_rl.config import FileConfig
+from htm_rl.common.utils import timed
 from htm_rl.envs.biogwlab.env import BioGwLabEnvironment
-from htm_rl.envs.biogwlab.wrappers.agent_position_provider import AgentPositionProvider
 from htm_rl.envs.env import Env, unwrap
 from htm_rl.recorders import (
-    AggregateRecorder, HeatmapRecorder, ValueMapRecorder, MapRecorder, DreamRecorder,
-    BaseRecorder, AnomalyMapRecorder, TDErrorMapRecorder, DreamingValueChangeProvider,
+    BaseOutput,
 )
 
 
+class ProgressPoint:
+    step: int
+    episode: int
+
+    def __init__(self):
+        self.step = 0
+        self.episode = 0
+
+    @property
+    def is_new_episode(self) -> bool:
+        return self.step == 0
+
+    def next_step(self):
+        self.step += 1
+
+    def start_new_episode(self):
+        self.step = 0
+        self.episode += 1
+
+
 class Experiment:
+    config: dict
+
     n_episodes: int
     print: dict
     wandb: dict
 
-    def __init__(self, n_episodes: int, print: dict, wandb: dict, **_):
-        self.n_episodes = n_episodes
-        self.print = print
-        self.wandb = wandb
+    env: Env
+    agent: Agent
+    progress: ProgressPoint
 
-    def run(self, config: FileConfig):
-        env = self.materialize_environment(config)
-        agent = self.materialize_agent(config, env)
-        env_shape = unwrap(env).shape
+    def __init__(self, config: dict):
+        self.config = config
 
-        handlers = []
-        if self.print['maps'] is not None:
-            handlers.append(MapRecorder(config, self.n_episodes))
+        self.n_episodes = config['n_episodes']
+        self.print = config['print']
+        self.wandb = config['wandb']
 
-        if self.print['heatmaps'] is not None:
-            env = AgentPositionProvider(env)
-            handlers.extend([
-                HeatmapRecorder(config, freq, env_shape)
-                for freq in self.print['heatmaps']
-            ])
+        debug_enabled = self.print['debug'] is not None
 
-        if self.print['debug'] is not None:
-            env = AgentPositionProvider(env)
-            agent = wrap(
-                agent,
-                TDErrorProvider,
-                AnomalyProvider, DreamingLengthProvider,
-            )
-            for freq in self.print['debug']:
-                aggregator = AggregateRecorder(config, freq)
-                handlers.extend([
-                    MapRecorder(config, freq, False, aggregator),
-                    HeatmapRecorder(config, freq, env_shape, aggregator),
-                    # ValueMapRecorder(config, freq, env_shape, 'value', aggregator),
-                    # ValueMapRecorder(config, freq, env_shape, 'value_exp', aggregator),
-                    DreamRecorder(config, freq, env_shape, aggregator),
-                    AnomalyMapRecorder(config, freq, env_shape, aggregator),
-                    TDErrorMapRecorder(config, freq, env_shape, aggregator),
-                    DreamingValueChangeProvider(config, freq, agent, env, aggregator),
-                    aggregator
-                ])
+        self.env = self.materialize_environment(
+            config['env'], config['env_seed'], config['envs'], debug_enabled
+        )
+        self.agent = self.materialize_agent(
+            config['agent'], config['agent_seed'], config['agents'], self.env, debug_enabled
+        )
+
+        self.progress = ProgressPoint()
+
+    def run(self):
+        env_shape = unwrap(self.env).shape
+        #
+        # handlers = []
+        # if self.print['maps'] is not None:
+        #     handlers.append(MapRecorder(self.config, self.n_episodes))
+        #
+        # if self.print['heatmaps'] is not None:
+        #     env = AgentPositionProvider(self.env)
+        #     handlers.extend([
+        #         HeatmapRecorder(config, freq, env_shape)
+        #         for freq in self.print['heatmaps']
+        #     ])
+        #
+        # if self.print['debug'] is not None:
+        #     env = AgentPositionProvider(env)
+        #     agent = wrap(
+        #         agent,
+        #         TDErrorProvider,
+        #         AnomalyProvider, DreamingLengthProvider,
+        #     )
+        #     for freq in self.print['debug']:
+        #         aggregator = ImageOutput(config, freq)
+        #         handlers.extend([
+        #             MapRecorder(config, freq, False, aggregator),
+        #             HeatmapRecorder(config, freq, env_shape, aggregator),
+        #             # ValueMapRecorder(config, freq, env_shape, 'value', aggregator),
+        #             # ValueMapRecorder(config, freq, env_shape, 'value_exp', aggregator),
+        #             DreamRecorder(config, freq, env_shape, aggregator),
+        #             AnomalyMapRecorder(config, freq, env_shape, aggregator),
+        #             TDErrorMapRecorder(config, freq, env_shape, aggregator),
+        #             DreamingValueChangeProvider(config, freq, agent, env, aggregator),
+        #             aggregator
+        #         ])
 
         train_stats = RunStats()
-        wandb_run = self.init_wandb_run(agent, config) if self.wandb['enabled'] else None
+        wandb_run = self.init_wandb_run()
 
-        for episode in trange(self.n_episodes):
-            (steps, reward), elapsed_time = self.run_episode(env, agent, handlers)
-            self.handle_episode(env, agent, episode, handlers)
+        for _ in trange(self.n_episodes):
+            (steps, reward), elapsed_time = self.run_episode()
             train_stats.append_stats(steps, reward, elapsed_time)
-            if self.wandb['enabled']:
+            self.progress.start_new_episode()
+
+            if wandb_run is not None:
                 wandb_run.log({
                     'steps': steps,
                     'reward': reward,
@@ -82,29 +116,28 @@ class Experiment:
         return train_stats
 
     @timed
-    def run_episode(self, env: Env, agent: Agent, handlers: list):
-        step = 0
+    def run_episode(self):
         total_reward = 0.
 
         while True:
-            reward, obs, first = env.observe()
-            if first and step > 0:
+            reward, obs, first = self.env.observe()
+            if first and self.progress.step > 0:
                 break
 
-            action = agent.act(reward, obs, first)
-            self.handle_step(env, agent, step, handlers)
-            env.act(action)
+            action = self.agent.act(reward, obs, first)
+            self.env.act(action)
 
-            step += 1
+            self.progress.next_step()
             total_reward += reward
 
-        return step, total_reward
+        return self.progress.step, total_reward
 
     @staticmethod
-    def materialize_agent(config: FileConfig, env: Env) -> Agent:
-        agent_name = config['agent']
-        seed: int = config['agent_seed']
-        agent_config: dict = config['agents'][agent_name]
+    def materialize_agent(
+            name: str, seed: int, agent_configs: dict[str, dict], env: Env,
+            debug_enabled: bool
+    ) -> Agent:
+        agent_config: dict = agent_configs[name]
 
         agent_type = agent_config['_type_']
         agent_config = filter_out_non_passable_items(agent_config)
@@ -113,35 +146,46 @@ class Experiment:
         elif agent_type == 'ucb':
             return UcbAgent(seed=seed, env=env, **agent_config)
         elif agent_type == 'svpn':
-            return inject_debug_tools(SvpnAgent)(seed=seed, env=env, **agent_config)
-            # return SvpnAgent(seed=seed, env=env, **agent_config)
+            ctor = inject_debug_tools(SvpnAgent, inject_or_not=debug_enabled)
+            return ctor(seed=seed, env=env, **agent_config)
         else:
             raise NameError(agent_type)
 
     @staticmethod
-    def materialize_environment(config: FileConfig) -> Env:
-        env_name = config['env']
-        seed: int = config['env_seed']
-        env_config: dict = config['envs'][env_name]
+    def materialize_environment(
+            name: str, seed: int, env_configs: dict[str, dict], debug_enabled: bool
+    ) -> Env:
+        env_config: dict = env_configs[name]
 
         env_type = env_config['_type_']
         env_config = filter_out_non_passable_items(env_config)
         if env_type == 'biogwlab':
-            return BioGwLabEnvironment(seed=seed, **env_config)
+            ctor = inject_debug_tools(BioGwLabEnvironment, inject_or_not=debug_enabled)
+            return ctor(seed=seed, **env_config)
         else:
             raise NameError(env_type)
 
-    def init_wandb_run(self, agent, config):
+    def init_wandb_run(self):
+        if not self.wandb['enabled']:
+            return None
+
         import wandb
         project = self.wandb['project']
-        assert project is not None, 'Either set up `project` to the experiment, or turn off wandb'
-        run = wandb.init(project=project, reinit=True, dir=config['base_dir'])
-        run.config.agent = {'name': config['agent'], 'type': agent.name, 'seed': config['agent_seed']}
-        run.config.environment = {'name': config['env'], 'seed': config['env_seed']}
+        assert project is not None, 'Wandb project name, set by `wandb.project` config field, is missing.'
+        run = wandb.init(project=project, reinit=True, dir=self.config['base_dir'])
+        run.config.agent = {
+            'name': self.config['agent'],
+            'type': self.agent.name,
+            'seed': self.config['agent_seed']
+        }
+        run.config.environment = {
+            'name': self.config['env'],
+            'seed': self.config['env_seed']
+        }
         return run
 
     def handle_episode(
-            self, env: Env, agent: Agent, episode: int, handlers: list[BaseRecorder]
+            self, env: Env, agent: Agent, episode: int, handlers: list[BaseOutput]
     ):
         env_info = env.get_info()
         agent_info = agent.get_info()
@@ -151,7 +195,7 @@ class Experiment:
             )
 
     def handle_step(
-            self, env: Env, agent: Agent, step: int, handlers: list[BaseRecorder]
+            self, env: Env, agent: Agent, step: int, handlers: list[BaseOutput]
     ):
         env_info = env.get_info()
         agent_info = agent.get_info()
