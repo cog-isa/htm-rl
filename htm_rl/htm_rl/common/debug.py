d300962d226edd376f7292e20c60c58f6f8727fe
Petr Kuderov <pkuderov@gmail.com>
2021-06-24 21:42:30 +0300

    make debug tools injection instance based
    Signed-off-by: Petr Kuderov <pkuderov@gmail.com>

--- htm_rl/htm_rl/common/debug.py
+++ htm_rl/htm_rl/common/debug.py
@@ -1,7 +1,54 @@
+from types import MethodType
 from typing import Any
 
 
-def inject_debug_tools(base_type, inject_or_not=True):
+def inject_debug_tools(obj, inject_or_not=True):
+    if isinstance(obj, type):
+        return inject_debug_tools_to_type(obj, inject_or_not)
+    else:
+        inject_debug_tools_to_obj(obj, inject_or_not)
+
+
+def inject_debug_tools_to_obj(obj, inject_or_not=True):
+    """Create subclass with added debugging helpers via dynamic inheritance."""
+
+    def set_breakpoint(self, method_name: str, callback):
+        self.unset_breakpoint(method_name, callback)
+        if method_name not in self._overrides:
+            self._overrides[method_name] = [getattr(self, method_name)]
+
+        self._overrides[method_name].append(callback)
+        self._apply_breakpoint(method_name)
+
+    def unset_breakpoint(self, method_name: str, callback):
+        if method_name not in self._overrides or callback not in self._overrides[method_name]:
+            return
+        self._overrides[method_name].remove(callback)
+        self._apply_breakpoint(method_name)
+
+    def _apply_breakpoint(self, method_name: str):
+        def wrap(breakpoint_callback, breakpointed_func):
+            def wrapper(*args, **kwargs):
+                return breakpoint_callback(self, breakpointed_func, *args, **kwargs)
+            return wrapper
+
+        overrides = self._overrides[method_name]
+        method = overrides[0]
+        for callback in overrides[1:]:
+            method = wrap(callback, method)
+
+        setattr(self, method_name, method)
+
+    if not inject_or_not or hasattr(obj, 'set_breakpoint'):
+        return
+
+    obj._overrides = dict()
+    setattr(obj, 'set_breakpoint', MethodType(set_breakpoint, obj))
+    setattr(obj, 'unset_breakpoint', MethodType(unset_breakpoint, obj))
+    setattr(obj, '_apply_breakpoint', MethodType(_apply_breakpoint, obj))
+
+
+def inject_debug_tools_to_type(base_type, inject_or_not=True):
     """Create subclass with added debugging helpers via dynamic inheritance."""
 
     class DynamicWrapper(base_type):
