cbcf30dbb4d770567b012ab1ce19131949f8925b
Petr Kuderov <pkuderov@gmail.com>
2021-06-27 22:35:51 +0300

    add state encoding scheme provider with decoding functionality

--- htm_rl/htm_rl/agents/svpn/debug/providers.py
+++ htm_rl/htm_rl/agents/svpn/debug/providers.py
@@ -1,15 +1,15 @@
-from typing import TYPE_CHECKING, Optional
+from typing import Optional
 
 import numpy as np
 
+from htm_rl.agents.svpn.agent import SvpnAgent
 from htm_rl.agents.svpn.debug.debugger import Debugger
+from htm_rl.common.sdr import SparseSdr
 from htm_rl.common.utils import isnone, ensure_list
+from htm_rl.envs.biogwlab.agent import Agent as EnvAgent
+from htm_rl.envs.biogwlab.environment import Environment
+from htm_rl.envs.biogwlab.module import EntityType
 from htm_rl.experiment import Experiment
-
-if TYPE_CHECKING:
-    from htm_rl.agents.svpn.agent import SvpnAgent
-    from htm_rl.envs.biogwlab.environment import Environment
-    from htm_rl.envs.biogwlab.agent import Agent as EnvAgent
 
 
 class AgentStateProvider(Debugger):
@@ -103,3 +103,49 @@ class EnvMapProvider(Debugger):
     @property
     def has_observation(self):
         return len(self.maps) > 1
+
+
+class StateEncodingProvider(Debugger):
+    agent: SvpnAgent
+    env: Environment
+
+    position_provider: AgentStateProvider
+
+    def __init__(self, experiment: Experiment):
+        super().__init__(experiment)
+        self.position_provider = AgentStateProvider(experiment)
+
+    def get_encoding_scheme(self) -> dict[tuple[int, int], SparseSdr]:
+        height, width = self.env.shape
+        obstacle_mask = self.env.aggregated_mask[EntityType.Obstacle]
+        encoding_scheme = dict()
+
+        for i in range(height):
+            for j in range(width):
+                if obstacle_mask[i, j]:
+                    continue
+                position = i, j
+                self.position_provider.overwrite(position)
+                observation = self.env.render()
+                encoding_scheme[position] = observation
+
+        self.position_provider.restore()
+        return encoding_scheme
+
+    @staticmethod
+    def decode_state(
+            state: SparseSdr,
+            encoding_scheme: dict[tuple[int, int], SparseSdr],
+            min_overlap_rate: float = .6
+    ) -> Optional[tuple[int, int]]:
+        best_match = None, 0.
+        state = set(state)
+        for position, state_ in encoding_scheme.items():
+            state_ = set(state_)
+            overlap = len(state & state_) / len(state)
+            if overlap < min_overlap_rate:
+                continue
+            if overlap > best_match[1]:
+                best_match = position, overlap
+
+        return best_match[0]
