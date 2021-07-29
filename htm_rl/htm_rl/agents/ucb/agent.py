e6801f4252eec9fbda4004907393e06469a090b1
Petr Kuderov <pkuderov@gmail.com>
2021-07-29 22:10:36 +0300

    WIP: refactor ucb agent to the new simplified object-based scheme

--- htm_rl/htm_rl/agents/ucb/agent.py
+++ htm_rl/htm_rl/agents/ucb/agent.py
@@ -1,22 +1,22 @@
-from typing import Dict, Optional, List
+from typing import Optional
+
+import numpy as np
 
 from htm_rl.agents.agent import Agent
-from htm_rl.agents.ucb.sparse_value_network import SparseValueNetwork
+from htm_rl.agents.q.eligibility_traces import EligibilityTraces
+from htm_rl.agents.q.qvn import QValueNetwork
+from htm_rl.agents.q.sa_encoder import SaEncoder
+from htm_rl.agents.ucb.ucb_estimator import UcbEstimator
 from htm_rl.common.sdr import SparseSdr
-from htm_rl.common.sdr_encoders import IntBucketEncoder, SdrConcatenator
 from htm_rl.envs.env import Env
-from htm_rl.htm_plugins.spatial_pooler import SpatialPooler
 
 
 class UcbAgent(Agent):
-    SparseValueNetwork = SparseValueNetwork
-
     n_actions: int
-    state_sp: SpatialPooler
-    action_encoder: IntBucketEncoder
-    sa_concatenator: SdrConcatenator
-    sa_sp: SpatialPooler
-    sqvn: SparseValueNetwork
+    sa_encoder: SaEncoder
+    Q: QValueNetwork
+    E_traces: Optional[EligibilityTraces]
+    ucb_estimate: UcbEstimator
 
     _current_sa_sdr: Optional[SparseSdr]
 
@@ -24,30 +24,19 @@ class UcbAgent(Agent):
             self,
             env: Env,
             seed: int,
-            sqvn: Dict,
-            state_sp: Dict,
-            action_encoder: Dict,
-            sa_sp: Dict
+            sa_encoder: dict,
+            qvn: dict,
+            ucb_estimate: dict,
+            eligibility_traces: dict = None,
     ):
-        self.state_sp = SpatialPooler(input_source=env, seed=seed, **state_sp)
-        action_encoder['bucket_size'] = max(
-            int(.75 * self.state_sp.n_active_bits),
-            action_encoder.get('bucket_size', 0)
-        )
-        self.action_encoder = IntBucketEncoder(n_values=env.n_actions, **action_encoder)
-        self.sa_concatenator = SdrConcatenator(input_sources=[
-            self.state_sp,
-            self.action_encoder
-        ])
-        self.sa_sp = SpatialPooler(input_source=self.sa_concatenator, seed=seed, **sa_sp)
-
-        self.sqvn = self.SparseValueNetwork(
-            cells_sdr_size=self.sa_sp.output_sdr_size,
-            seed=seed,
-            **sqvn
-        )
         self.n_actions = env.n_actions
-        self._current_sa_sdr = None
+        self.sa_encoder = SaEncoder(env, seed, **sa_encoder)
+        self.Q = QValueNetwork(self.sa_encoder.output_sdr_size, seed, **qvn)
+        self.E_traces = EligibilityTraces(
+            self.sa_encoder.output_sdr_size,
+            **eligibility_traces
+        )
+        self.ucb_estimate = UcbEstimator(self.sa_encoder.output_sdr_size, **ucb_estimate)
 
     @property
     def name(self):
@@ -55,51 +44,31 @@ class UcbAgent(Agent):
 
     def act(self, reward: float, state: SparseSdr, first: bool):
         if first:
-            self._on_new_episode()
+            self.on_new_episode()
 
-        s = self.state_sp.compute(state, learn=True)
-        actions_sa_sdr = self._encode_actions(s, learn=True)
+        s = self.sa_encoder.encode_state(state, learn=True)
+        actions_sa_sdr = self.sa_encoder.encode_actions(s, learn=True)
+
+        action_values = self.Q.values(actions_sa_sdr)
         if not first:
-            # process feedback
-            self._learn_step(
-                prev_sa_sdr=self._current_sa_sdr,
-                reward=reward,
-                actions_sa_sdr=actions_sa_sdr
+            # Q-learning step
+            greedy_action = np.argmax(action_values)
+            greedy_sa_sdr = actions_sa_sdr[greedy_action]
+            self.Q.update(
+                sa=self._current_sa_sdr, reward=reward,
+                sa_next=greedy_sa_sdr,
+                E_traces=self.E_traces.E
             )
 
-        action = self.sqvn.choose(actions_sa_sdr)
+        # choose action
+        ucb_values = self.ucb_estimate.ucb_terms(actions_sa_sdr)
+        action = np.argmax(action_values + ucb_values)
+
         self._current_sa_sdr = actions_sa_sdr[action]
+        self.ucb_estimate.update(self._current_sa_sdr)
         return action
 
-    def _learn_step(
-            self, prev_sa_sdr, reward, actions_sa_sdr, td_lambda: bool = True,
-            update_visit_count=True
-    ):
-        greedy_action = self.sqvn.choose(actions_sa_sdr, greedy=True)
-        greedy_sa_sdr = actions_sa_sdr[greedy_action]
-
-        self.sqvn.update(
-            sa=prev_sa_sdr,
-            reward=reward,
-            sa_next=greedy_sa_sdr,
-            td_lambda=td_lambda,
-            update_visit_count=update_visit_count
-        )
-
-    def _on_new_episode(self):
-        self.sqvn.reset()
-        self.sqvn.decay_learning_factors()
-
-    def _encode_actions(self, s: SparseSdr, learn: bool) -> List[SparseSdr]:
-        actions = []
-        for action in range(self.n_actions):
-            sa_sdr = self._encode_sa(s, action, learn=learn)
-            actions.append(sa_sdr)
-
-        return actions
-
-    def _encode_sa(self, s: SparseSdr, action: int, learn: bool) -> SparseSdr:
-        a = self.action_encoder.encode(action)
-        s_a = self.sa_concatenator.concatenate(s, a)
-        sa = self.sa_sp.compute(s_a, learn=learn)
-        return sa
+    def on_new_episode(self):
+        self.Q.decay_learning_factors()
+        self.E_traces.reset()
+        self.ucb_estimate.decay_learning_factors()
--- htm_rl/htm_rl/agents/ucb/sparse_value_network.py
+++ htm_rl/htm_rl/agents/ucb/sparse_value_network.py
@@ -7,61 +7,6 @@ from htm_rl.common.sdr import SparseSdr
 from htm_rl.common.utils import update_exp_trace, exp_decay
 
 
-class UcbEstimator:
-    visit_decay: float
-    discount_factor: float
-    learning_rate: Tuple[float, float]
-    ucb_exploration_factor: Tuple[float, float]
-
-    # you should not to read it literally cause it's affected by exp MA window
-    cell_visit_count: np.ndarray
-
-    def __init__(
-            self, cells_sdr_size: int,
-            visit_decay: float,
-            discount_factor: float,
-            learning_rate: Tuple[float, float],
-            ucb_exploration_factor: Tuple[float, float]
-    ):
-        self.visit_decay = visit_decay
-        self.discount_factor = discount_factor
-        self.learning_rate = learning_rate
-        self.ucb_exploration_factor = ucb_exploration_factor
-
-        self.cell_visit_count = np.full(cells_sdr_size, 1., dtype=np.float)
-
-    def ucb_terms(self, xs: List[SparseSdr]) -> np.ndarray:
-        total_visits = self.total_visits(xs)
-        return np.array([self.ucb_term(x, total_visits) for x in xs])
-
-    def update(self, sa: SparseSdr):
-        update_exp_trace(self.cell_visit_count, sa, self.visit_decay)
-
-    # noinspection PyPep8Naming
-    def ucb_term(self, x, total_visits) -> float:
-        # NB: T regards to a total options (xs) visits, N - to just a cells in x
-        # but we consider here as if each cell was a single
-        # representative of an option
-        T = total_visits
-        N = self.cell_visit_count[x]
-        cp = self.ucb_exploration_factor[0]
-
-        ucb = cp * np.sqrt(2 * np.log(T + 1) / (N + 1))
-        return ucb.mean()
-
-    def total_visits(self, xs: List[SparseSdr]) -> int:
-        # option visit count: avg visit count of its cells
-        return sum(
-            self.cell_visit_count[x].mean()
-            for x in xs
-            if len(x) > 0
-        )
-
-    def decay_learning_factors(self):
-        self.ucb_exploration_factor = exp_decay(self.ucb_exploration_factor)
-        self.learning_rate = exp_decay(self.learning_rate)
-
-
 class SparseValueNetwork:
     trace_decay: float
     visit_decay: float
