a6c0e566e9be9fe62a0490dac4b0147137b72405
Petr Kuderov <pkuderov@gmail.com>
2021-08-21 11:17:21 +0300

    update dreamer to be exact copy of the dreamer0

--- htm_rl/htm_rl/agents/dreamer/agent.py
+++ htm_rl/htm_rl/agents/dreamer/agent.py
@@ -5,12 +5,13 @@ from numpy.random import Generator
 
 from htm_rl.agents.agent import Agent
 from htm_rl.agents.dreamer.dreaming_double import DreamingDouble
-from htm_rl.agents.q.agent import softmax
+from htm_rl.agents.q.agent import softmax, make_sa_encoder
 from htm_rl.agents.q.eligibility_traces import EligibilityTraces
 from htm_rl.agents.q.qvn import QValueNetwork
 from htm_rl.agents.q.sa_encoder import SaEncoder
+from htm_rl.agents.qmb.agent import make_transition_model
 from htm_rl.agents.qmb.reward_model import RewardModel
-from htm_rl.agents.qmb.transition_model import SsaTransitionModel
+from htm_rl.agents.qmb.transition_model import TransitionModel
 from htm_rl.agents.ucb.ucb_estimator import UcbEstimator
 from htm_rl.common.sdr import SparseSdr
 from htm_rl.common.utils import exp_decay, exp_sum
@@ -23,7 +24,7 @@ class DreamerAgent(Agent):
     Q: QValueNetwork
     E_traces: Optional[EligibilityTraces]
 
-    sa_transition_model: SsaTransitionModel
+    transition_model: TransitionModel
     reward_model: RewardModel
 
     im_weight: tuple[float, float]
@@ -45,7 +46,6 @@ class DreamerAgent(Agent):
             self,
             env: Env,
             seed: int,
-            sa_encoder: dict,
             qvn: dict,
             reward_model: dict,
             transition_model: dict,
@@ -56,17 +56,17 @@ class DreamerAgent(Agent):
             exploration_eps: tuple[float, float] = None,
             softmax_enabled: bool = False,
             ucb_estimate: dict = None,
+            sa_encoder: dict = None,
     ):
         self.n_actions = env.n_actions
-        self.sa_encoder = SaEncoder(env, seed, **sa_encoder)
+        self.sa_encoder = make_sa_encoder(env, seed, sa_encoder)
         self.Q = QValueNetwork(self.sa_encoder.output_sdr_size, seed, **qvn)
         self.E_traces = EligibilityTraces(
             self.sa_encoder.output_sdr_size,
             **eligibility_traces
         )
-        self.sa_transition_model = SsaTransitionModel(
-            self.sa_encoder.state_sp, self.sa_encoder.sa_sp,
-            **transition_model
+        self.transition_model = make_transition_model(
+            self.sa_encoder, transition_model
         )
         self.reward_model = RewardModel(self.sa_encoder.output_sdr_size, **reward_model)
         self.exploration_eps = exploration_eps
@@ -92,7 +92,7 @@ class DreamerAgent(Agent):
         if first:
             self.on_new_episode()
 
-        x = (1 - self.sa_transition_model.recall)**2
+        x = (1 - self.transition_model.recall) ** 2
         im_reward = self.im_weight[0] * x
 
         s = self.sa_encoder.encode_state(state, learn=True)
@@ -104,6 +104,7 @@ class DreamerAgent(Agent):
             self.reward_model.update(s, reward)
             # Q-learning step
             greedy_sa_sdr = actions_sa_sdr[greedy_action]
+            self.E_traces.update(self._current_sa_sdr)
             self.Q.update(
                 sa=self._current_sa_sdr, reward=reward + im_reward,
                 sa_next=greedy_sa_sdr,
@@ -128,7 +129,10 @@ class DreamerAgent(Agent):
             action = np.argmax(action_values + ucb_values)
 
         self._current_sa_sdr = actions_sa_sdr[action]
-        self.sa_transition_model.process(s, self._current_sa_sdr, learn=True)
+        self.transition_model.process(
+            self.transition_model.preprocess(s, self._current_sa_sdr),
+            learn=True
+        )
         if self.ucb_estimate is not None:
             self.ucb_estimate.update(self._current_sa_sdr)
         return action
@@ -138,7 +142,7 @@ class DreamerAgent(Agent):
         self.E_traces.reset()
         if self.exploration_eps is not None:
             self.exploration_eps = exp_decay(self.exploration_eps)
-        self.sa_transition_model.reset()
+        self.transition_model.reset()
         self.reward_model.decay_learning_factors()
         self.im_weight = exp_decay(self.im_weight)
         if self.ucb_estimate is not None:
