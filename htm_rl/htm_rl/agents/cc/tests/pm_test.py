eeb926c2a2df71b7626a40350532ace04fa6d42d
gsys <zhekauran@gmail.com>
2021-08-28 09:31:37 +0300

    add simpler version of pattern memory

--- htm_rl/htm_rl/agents/cc/tests/pm_test.py
+++ htm_rl/htm_rl/agents/cc/tests/pm_test.py
@@ -1,6 +1,7 @@
 from htm_rl.agents.cc.memory import PatternMemory
 import numpy as np
 from htm.bindings.sdr import SDR
+from tqdm import tqdm
 
 EPS = 1e-12
 
@@ -11,27 +12,26 @@ def get_labels(pm: PatternMemory, data, input_size):
     for i, item in enumerate(data):
         input_pattern.sparse = item
         labels[i] = pm.compute(input_pattern, False)
-        if labels[i] is None:
-            raise ValueError('Pattern have not learned!')
     return labels
 
 
-def train(pm: PatternMemory, data, epochs, input_size, noise=0):
+def train(pm: PatternMemory, data, epochs, input_size, noise=0.0):
     input_pattern = SDR(input_size)
     indices = np.arange(len(data))
-    for epoch in range(epochs):
+    for epoch in tqdm(range(epochs)):
         np.random.shuffle(indices)
         for i in indices:
             if noise > 0:
-                bits_to_remove = np.random.sample(data[i], noise)
-                bits_to_add = np.random.sample(np.arange(input_size), noise)
+                n_bits = int(noise * len(data[i]))
+                bits_to_remove = np.random.choice(data[i], n_bits, replace=False)
+                bits_to_add = np.random.choice(np.arange(input_size), n_bits, replace=False)
                 noisy_sample = np.setdiff1d(data[i], bits_to_remove)
                 noisy_sample = np.union1d(noisy_sample, bits_to_add)
             else:
                 noisy_sample = data[i]
             input_pattern.sparse = noisy_sample
             pm.compute(input_pattern, True)
-        print(f'epoch {epoch}: {get_labels(pm, data, input_size)}')
+        # print(f'epoch {epoch}: {get_labels(pm, data, input_size)}')
 
     labels = get_labels(pm, data, input_size)
     return labels
@@ -39,9 +39,12 @@ def train(pm: PatternMemory, data, epochs, input_size, noise=0):
 
 def test_retrieval(pm: PatternMemory, data, labels):
     iou = list()
-    for label, item in zip(labels, data):
-        pattern = pm.get_pattern(label)
-        iou.append(np.intersect1d(pattern, item).size/(np.union1d(pattern, item).size + EPS))
+    for i, item in enumerate(data):
+        if labels[i] is not None:
+            pattern = pm.get_pattern(labels[i])
+            iou.append(np.intersect1d(pattern, item).size/(np.union1d(pattern, item).size + EPS))
+        else:
+            iou.append(0)
     return sum(iou)/len(iou)
 
 
@@ -52,28 +55,23 @@ def generate_data(input_size, n_patterns, sparsity):
 
 def main():
     input_size = 1000
-    epochs = 5
+    epochs = 20
     seed = 5436
-    n_patterns = 10
-    sparsity = 0.1
+    n_patterns = 1000
+    sparsity = 0.05
     config = dict(
-        input_size=1000,
-        num_cells=10,
-        radius=4,
-        min_distance=0.05,
+        input_size=input_size,
+        max_segments=1000,
+        min_distance=0.1,
         permanence_increment=0.1,
-        permanence_decrement=0.1,
+        permanence_decrement=0.01,
         segment_decrement=0.1,
         permanence_connected_threshold=0.5,
-        activation_threshold=1,
-        activity_period=100,
-        min_activity_threshold=0.001,
-        boost_factor=2,
         seed=seed
     )
     data = generate_data(input_size, n_patterns, sparsity)
     pm = PatternMemory(**config)
-    labels = train(pm, data, epochs, input_size)
+    labels = train(pm, data, epochs, input_size, noise=0.09)
     mean_iou = test_retrieval(pm, data, labels)
 
     print(mean_iou)
