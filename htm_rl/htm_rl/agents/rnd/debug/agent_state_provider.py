d20063b4031e9aa4198a96761e0173d04fb0452f
Petr Kuderov <pkuderov@gmail.com>
2021-09-03 13:31:34 +0300

    refine agent state provider

--- htm_rl/htm_rl/agents/rnd/debug/agent_state_provider.py
+++ htm_rl/htm_rl/agents/rnd/debug/agent_state_provider.py
@@ -1,6 +1,5 @@
 from htm_rl.agents.rnd.debug.debugger import Debugger
 from htm_rl.common.utils import isnone
-from htm_rl.envs.biogwlab.agent import Agent as EnvAgent
 from htm_rl.envs.biogwlab.environment import Environment
 from htm_rl.scenarios.standard.scenario import Scenario
 
@@ -13,21 +12,16 @@ class AgentStateProvider(Debugger):
         self.origin = None
 
     @property
-    def env_agent(self) -> EnvAgent:
-        # noinspection PyTypeChecker
-        return self.env.entities['agent']
-
-    @property
     def state(self):
         return self.position, self.view_direction
 
     @property
     def position(self):
-        return self.env_agent.position
+        return self.env.agent.position
 
     @property
     def view_direction(self):
-        return self.env_agent.view_direction
+        return self.env.agent.view_direction
 
     def overwrite(self, position=None, view_direction=None):
         if self.origin is None:
@@ -41,5 +35,5 @@ class AgentStateProvider(Debugger):
         self.origin = None
 
     def _set(self, position, view_direction):
-        self.env_agent.position = isnone(position, self.env_agent.position)
-        self.env_agent.view_direction = isnone(view_direction, self.env_agent.view_direction)
+        self.env.agent.position = isnone(position, self.env.agent.position)
+        self.env.agent.view_direction = isnone(view_direction, self.env.agent.view_direction)
