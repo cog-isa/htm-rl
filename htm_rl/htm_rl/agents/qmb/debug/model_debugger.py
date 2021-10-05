92e0bf21bf882f77d4346c324b6513eb40b686d2
Petr Kuderov <pkuderov@gmail.com>
2021-10-06 00:48:24 +0300

    fix model debugger, add more debug seeds for 8x8 obs

--- htm_rl/htm_rl/agents/qmb/debug/model_debugger.py
+++ htm_rl/htm_rl/agents/qmb/debug/model_debugger.py
@@ -5,6 +5,7 @@ from htm_rl.agents.qmb.debug.anomaly_map_provider import AnomalyMapProvider
 from htm_rl.agents.rnd.debug.debugger import Debugger
 from htm_rl.agents.rnd.debug.env_map_provider import EnvMapProvider
 from htm_rl.agents.rnd.debug.trajectory_tracker import TrajectoryTracker
+from htm_rl.common.debug import inject_debug_tools
 from htm_rl.common.plot_utils import plot_grid_images
 from htm_rl.envs.biogwlab.environment import Environment
 from htm_rl.scenarios.debug_output import ImageOutput
@@ -16,7 +17,7 @@ class ModelDebugger(Debugger):
     env: Environment
     agent: QModelBasedAgent
 
-    def __init__(self, scenario: Scenario, images: bool):
+    def __init__(self, scenario: Scenario, print_images: bool):
         super().__init__(scenario)
 
         self.output_renderer = ImageOutput(scenario.config)
@@ -25,21 +26,23 @@ class ModelDebugger(Debugger):
         self.state_encoding_provider = StateEncodingProvider(scenario)
         self.q_map_provider = QMapProvider(scenario, self.state_encoding_provider)
         self.anomaly_map_provider = AnomalyMapProvider(scenario, self.state_encoding_provider)
-        self.print_images = images
+        self.print_images = print_images
 
+        inject_debug_tools(self.progress)
         # noinspection PyUnresolvedReferences
         self.progress.set_breakpoint('end_episode', self.on_end_episode)
 
     # noinspection PyUnusedLocal
     def on_end_episode(self, pp: ProgressPoint, func, *args, **kwargs):
-        if self.print_images:
-            self.state_encoding_provider.encoding_scheme = None
+        if self.print_images and self.agent.train:
+            output_filename = self.get_episode_filename(pp, self.train_eval_mark)
+
+            self.state_encoding_provider.reset()
             self.env_map_provider.print_map(self.output_renderer, n=1)
             self.q_map_provider.print_maps(self.output_renderer, q=True, v=True)
             self.anomaly_map_provider.print_map(self.output_renderer)
             self.trajectory_tracker.print_map(self.output_renderer)
 
-            output_filename = self.get_episode_filename(pp, self.train_eval_mark)
             output = self.output_renderer.flush(output_filename)
             plot_grid_images(show=False, cols_per_row=3, **output)
 
