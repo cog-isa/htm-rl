79bd45d6a25fee37efc5c1461b0cdc7d2c9e304e
Arkol7 <ar.ko.latyshev@gmail.com>
2021-11-25 23:47:05 +0300

    add v1 complex length sum

--- htm_rl/htm_rl/experiments/v1/v1.py
+++ htm_rl/htm_rl/experiments/v1/v1.py
@@ -74,6 +74,17 @@ def create_gabor_convolution(
     return convolution
 
 
+def conv2d(signal_: np.ndarray, filter_: np.ndarray) -> np.ndarray:
+    h, w = filter_.shape
+    shape = (h, w)
+    signal_ = np.pad(signal_, ((h // 2, h - h // 2 - 1), (w // 2, w - w // 2 - 1)),
+                     'constant', constant_values=0)
+    s = shape + tuple(np.subtract(signal_.shape, shape) + 1)
+    strd = np.lib.stride_tricks.as_strided
+    sub_matr = strd(signal_, shape=s, strides=signal_.strides * 2)
+    return np.einsum('ij,ijkl->kl', filter_, sub_matr)
+
+
 def kWTA(preactivation: np.ndarray, activity_level: float) -> np.ndarray:
     # 1st step kWTA
     active_cells = np.argmax(preactivation, axis=0)
@@ -100,15 +111,15 @@ def kWTA(preactivation: np.ndarray, activity_level: float) -> np.ndarray:
 class V1Simple:
 
     def __init__(self,
-                 g_kernel_size: int,
-                 g_stride: int,
-                 g_pad: int,
-                 g_sigma_x: float,
-                 g_sigma_y: float,
-                 g_lambda_: float,
-                 g_filters: int,
-                 activity_level: float
-                 ):
+        g_kernel_size: int,
+        g_stride: int,
+        g_pad: int,
+        g_sigma_x: float,
+        g_sigma_y: float,
+        g_lambda_: float,
+        g_filters: int,
+        activity_level: float
+    ):
         self.convolution = create_gabor_convolution(
             g_kernel_size, g_stride, g_pad, g_sigma_x,
             g_sigma_y, g_lambda_, g_filters
@@ -157,12 +168,12 @@ class V1Simple:
 class V1SimpleMax:
 
     def __init__(self,
-                 g_kernel_size: int,
-                 g_stride: int,
-                 g_sigma: float,
-                 input_shape: tuple[int, int, int],
-                 activity_level: float,
-                 ):
+        g_kernel_size: int,
+        g_stride: int,
+        g_sigma: float,
+        input_shape: tuple[int, int, int],
+        activity_level: float,
+    ):
         self.activity_level = activity_level
         self.gaus_filter, wi, wj = create_gaus_filter(g_kernel_size, g_sigma)
         pad_w = (-np.min(wj), np.max(wj))
@@ -183,9 +194,58 @@ class V1SimpleMax:
         self.i = ii[:, :, np.newaxis] + wi.flatten()
         self.j = jj[:, :, np.newaxis] + wj.flatten()
 
-    def compute(self, input: np.ndarray) -> np.ndarray:
+    def compute(self, input: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         input_pad = np.pad(input, ((0, 0), *self.pad))
         preactivation = input_pad[:, self.i, self.j] * self.gaus_filter
         preactivation = preactivation.max(axis=3)
         activation = kWTA(preactivation, self.activity_level)
+        return activation, preactivation
+
+class V1Complex:
+
+    def __init__(self,
+        g_kernel_size: int,
+        g_stride: int,
+        g_sigma: float,
+        input_shape: tuple[int, int, int],
+        activity_level: float,
+    ):
+        self.activity_level = activity_level
+        self.v1_simple_max = V1SimpleMax(
+            g_kernel_size, g_stride, g_sigma, input_shape, activity_level
+        )
+        self.length_filters = [
+            np.array([[1 / 3], [1 / 3], [1 / 3]]),
+            np.array(
+                [[0, 0, 1 / 3],
+                 [0, 1 / 3, 0],
+                 [1 / 3, 0, 0]]
+            ),
+            np.array([[1/3, 1/3, 1/3]]),
+            np.array(
+                [[1 / 3, 0, 0],
+                 [0, 1 / 3, 0],
+                 [0, 0, 1 / 3]]
+            )
+        ]
+
+    def max_polarity(self, input: np.ndarray) -> np.ndarray:
+        num_filteres = input.shape[0]
+        angles = num_filteres // 2
+        output = np.zeros((angles, input.shape[1], input.shape[2]))
+        for i in range(angles):
+            output[i] = input[[i, i+angles]].max(axis=0)
+        return output
+
+    def length_sum(self, input: np.ndarray) -> np.ndarray:
+        preactivation = np.zeros_like(input)
+        for i, filter_ in enumerate(self.length_filters):
+            preactivation[i] = conv2d(input[i], filter_)
+        activation = kWTA(preactivation, self.activity_level)
         return activation
+
+    def compute(self, input: np.ndarray) -> np.ndarray:
+        v1simplemax, preactivation = self.v1_simple_max.compute(input)
+        angles_only_preact = self.max_polarity(preactivation)
+        v1lensum = self.length_sum(angles_only_preact)
+        return np.concatenate((v1simplemax, v1lensum), axis=0)
