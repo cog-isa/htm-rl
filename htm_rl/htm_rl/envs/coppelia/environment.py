3cd982bcd33236bb425d5af0bf5e391581b4b23d
gsys <zhekauran@gmail.com>
2021-11-27 13:35:59 +0300

    add reward and observations; change pulse environment interface

--- htm_rl/htm_rl/envs/coppelia/environment.py
+++ htm_rl/htm_rl/envs/coppelia/environment.py
@@ -1,6 +1,7 @@
 from os.path import dirname, join, abspath
 from pyrep import PyRep
 from pyrep.objects.vision_sensor import VisionSensor
+from pyrep.objects.force_sensor import ForceSensor
 from pyrep.objects.shape import Shape
 from arm import Pulse75
 import numpy as np
@@ -13,37 +14,75 @@ EPISODE_LENGTH = 10
 EPS = 0.01
 
 
-class SimEnv:
-
-    def __init__(self, scene_file: str):
+class PulseEnv:
+    def __init__(self,
+                 scene_file: str,
+                 observation: list[str],
+                 n_sim_steps_for_action: int,
+                 action_cost: float,
+                 goal_reward: float,
+                 position_threshold: float,
+                 change_position: bool,
+                 headless=False,
+                 seed=None):
         self.pr = PyRep()
-        self.pr.launch(scene_file, headless=False)
+        self.pr.launch(scene_file, headless=headless)
         self.pr.start()
         self.agent = Pulse75()
         self.agent.set_control_loop_enabled(True)
         self.agent.set_motor_locked_at_zero_velocity(True)
         self.target = Shape('target')
+        self.tip = ForceSensor('pulse75_connection')
         self.initial_joint_positions = self.agent.get_joint_positions()
         self.camera = VisionSensor('camera')
+        self.observation = set(observation)
+        self.is_first = True
+        self.n_sim_steps_for_action = n_sim_steps_for_action
+        self.action_cost = action_cost
+        self.goal_reward = goal_reward
+        self.position_threshold = position_threshold
+        self.change_position = change_position
+        self.rng = np.random.default_rng(seed)
 
     def reset(self) -> np.ndarray:
         # Get a random position within a cuboid and set the target position
-        pos = list(np.random.uniform(POS_MIN, POS_MAX))
-        self.target.set_position(pos)
+        if self.change_position:
+            pos = list(self.rng.uniform(POS_MIN, POS_MAX))
+            self.target.set_position(pos)
+
         self.agent.set_joint_positions(self.initial_joint_positions, disable_dynamics=True)
+        self.is_first = True
         return self.camera.capture_rgb()
 
-    def step(self, action: list[float]) -> np.ndarray:
+    def act(self, action: list[float]):
+        self.is_first = False
         self.agent.set_joint_target_positions(action)
-        done = False
-        while not done:
+        for step in range(self.n_sim_steps_for_action):
             self.pr.step()
-            current_pos = self.agent.get_joint_positions()
-            done = True
-            for i, a in enumerate(action):
-                if np.abs(a - current_pos[i]) > EPS:
-                    done = False
-        return self.camera.capture_rgb()
+
+    def observe(self):
+        obs = list()
+        if 'camera' in self.observation:
+            obs.append(self.camera.capture_rgb())
+        if 'joint_pos' in self.observation:
+            obs.append(self.agent.get_joint_positions())
+        if 'joint_vel' in self.observation:
+            obs.append(self.agent.get_joint_velocities())
+        if 'target_pos' in self.observation:
+            obs.append(self.target.get_position())
+        if 'target_vel' in self.observation:
+            obs.append(self.target.get_velocity())
+
+        reward = self.action_cost
+        x, y, z = self.tip.get_position()
+        tx, ty, tz = self.target.get_position()
+        if ((abs(x - tx) < self.position_threshold) and
+                (abs(y - ty) < self.position_threshold) and
+                (abs(z - tz) < self.position_threshold)):
+            reward += self.goal_reward
+            self.reset()
+
+        return obs, reward, self.is_first
 
     def shutdown(self):
         self.pr.stop()
@@ -51,17 +90,18 @@ class SimEnv:
 
 
 if __name__ == '__main__':
-    env = SimEnv(SCENE_FILE)
+    env = PulseEnv(SCENE_FILE, ['force'], 2, -0.1, 1)
     for e in range(EPISODES):
         print('Starting episode %d' % e)
-        state = env.reset()
+        env.reset()
         for i in range(EPISODE_LENGTH):
             print(f'Step {i}')
             action = list(np.random.uniform(-1.0, 1.0, size=(6,)))
-            state = env.step(action)
+            env.act(action)
+            state = env.observe()
+            print(f'pos {env.tip.get_position()}')
             # save images from camera
             # plt.imshow(state)
             # plt.savefig(join(dirname(abspath(__file__)), f'image_{e}_{i}.png'))
-
     print('Done!')
     env.shutdown()
