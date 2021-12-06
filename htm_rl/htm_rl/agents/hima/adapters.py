4010500453cacafbff83d3dd1ae1c0e409634912
gsys <zhekauran@gmail.com>
2021-12-06 14:37:29 +0300

    add observation adapters

--- htm_rl/htm_rl/agents/hima/adapters.py
+++ htm_rl/htm_rl/agents/hima/adapters.py
@@ -1,13 +1,15 @@
 from typing import Union
 
 from htm_rl.common.sdr_decoders import DecoderStack, IntBucketDecoder
+from htm_rl.common.sdr_encoders import RangeDynamicEncoder, VectorDynamicEncoder
+from htm_rl.envs.coppelia.environment import PulseEnv
 
 import numpy as np
 
 
-class PulseAdapter:
+class PulseActionAdapter:
     def __init__(self,
-                 environment,
+                 environment: PulseEnv,
                  n_joints,
                  speed_delta,
                  time_delta,
@@ -26,22 +28,22 @@ class PulseAdapter:
                                  bucket_size,
                                  default_value=default_value,
                                  seed=seed),
-                bit_range=(shift, shift+3*bucket_size))
-            shift += 3*bucket_size
+                bit_range=(shift, shift + 3 * bucket_size))
+            shift += 3 * bucket_size
 
         self.current_speeds = np.zeros(n_joints)
-        self.speed_deltas = np.array([0, speed_delta, -speed_delta])
+        self.speed_deltas = np.array([0, speed_delta / 180, -speed_delta / 180])
 
     def adapt(self, action):
         actions = self.decoder_stack.decode(action)
-        current_angles = self.environment.get_current_angles()
+        current_angles = self.environment.agent.get_joint_positions()
 
         # speed update
         speed_deltas = self.speed_deltas[actions]
         self.current_speeds += speed_deltas
 
         # angle update
-        next_angles = current_angles + self.current_speeds*self.time_delta
+        next_angles = current_angles + self.current_speeds * self.time_delta
 
         return next_angles
 
@@ -49,7 +51,87 @@ class PulseAdapter:
         self.current_speeds = np.zeros_like(self.current_speeds)
 
 
-class BioGwLabAdapter:
+class PulseObsAdapter:
+    def __init__(self, environment: PulseEnv, config):
+        self.environment = environment
+        self.n_joints = self.environment.agent.get_joint_count()
+        # setup encoders
+        self.encoders = dict()
+        self.observations = list()
+        if 'camera' in self.environment.observation:
+            self.encoders['camera'] = None
+            self.observations.append('camera')
+        if 'joint_pos' in self.environment.observation:
+            joint_pos_range = self.environment.agent.get_joint_intervals()[1][0]
+            joint_vel_limit = self.environment.agent.get_joint_upper_velocity_limits()[0]
+            self.encoders['joint_pos'] = VectorDynamicEncoder(
+                self.n_joints,
+                RangeDynamicEncoder(max_value=joint_pos_range[1],
+                                    min_value=joint_pos_range[0],
+                                    max_speed=joint_vel_limit,
+                                    min_speed=-joint_vel_limit,
+                                    cyclic=True,
+                                    **config['joint_pos'])
+            )
+            self.observations.append('joint_pos')
+        if 'joint_vel' in self.environment.observation:
+            joint_vel_limit = self.environment.agent.get_joint_upper_velocity_limits()[0]
+            self.encoders['joint_vel'] = VectorDynamicEncoder(
+                self.n_joints,
+                RangeDynamicEncoder(
+                    max_value=joint_vel_limit,
+                    min_value=-joint_vel_limit,
+                    max_speed=1,
+                    min_speed=0,
+                    cyclic=True,
+                    **config['joint_vel'])
+            )
+            self.observations.append('joint_vel')
+        if 'target_pos' in self.environment.observation:
+            self.encoders['target_pos'] = VectorDynamicEncoder(
+                3,
+                RangeDynamicEncoder(
+                    cyclic=False,
+                    **config['target_pos'])
+            )
+            self.observations.append('target_pos')
+        if 'target_vel' in self.environment.observation:
+            self.encoders['target_vel'] = VectorDynamicEncoder(
+                3,
+                RangeDynamicEncoder(
+                    max_speed=1,
+                    min_speed=0,
+                    cyclic=False,
+                    **config['target_vel'])
+            )
+            self.observations.append('target_vel')
+
+    def adapt(self, obs):
+        outputs = list()
+        shift = 0
+        for i, obs_type in enumerate(self.observations):
+            encoder = self.encoders[obs_type]
+            if obs_type is 'camera':
+                sparse = np.empty(0)
+            elif obs_type is 'joint_pos':
+                sparse = encoder.encode(obs[i], self.environment.agent.get_joint_velocities())
+            elif obs_type is 'joint_vel':
+                sparse = encoder.encode(obs[i], np.zeros_like(obs[i]))
+            elif obs_type is 'target_pos':
+                sparse = encoder.encode(obs[i], self.environment.target.get_velocity())
+            elif obs_type is 'target_vel':
+                sparse = encoder.encode(obs[i], np.zeros_like(obs[i]))
+            else:
+                raise ValueError
+
+            sparse += shift
+            outputs.append(sparse)
+            if encoder is not None:
+                shift += encoder.output_sdr_size
+        return np.concatenate(outputs)
+
+
+class BioGwLabActionAdapter:
     def __init__(self, n_actions, bucket_size, default_value, seed):
         self.n_actions = n_actions
         self.decoder = IntBucketDecoder(n_actions, bucket_size, default_value=default_value, seed=seed)
@@ -59,3 +141,8 @@ class BioGwLabAdapter:
 
     def reset(self):
         pass
+
+
+class BioGwLabObsAdapter:
+    def adapt(self, obs):
+        return obs
