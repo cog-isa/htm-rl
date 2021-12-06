fb436c62808abda19f894dffdb4da3b76bc4f564
gsys <zhekauran@gmail.com>
2021-12-06 21:17:13 +0300

    compatability issues

--- htm_rl/htm_rl/agents/hima/runner.py
+++ htm_rl/htm_rl/agents/hima/runner.py
@@ -77,7 +77,7 @@ class HIMAgentRunner:
         elif config['environment_type'] == 'pulse':
             self.environment = PulseEnv(**config['environment'])
             self.action_adapter = PulseActionAdapter(self.environment, **config['pulse_action_adapter'])
-            self.observation_adapter = PulseObsAdapter(self.environment, config['pulse_obs_adapter'])
+            self.observation_adapter = PulseObsAdapter(self.environment, config['pulse_observation_adapter'])
         else:
             raise ValueError(f'Unknown environment type: {config["environment_type"]}! ')
 
@@ -110,11 +110,16 @@ class HIMAgentRunner:
         self.path_to_store_logs = config['path_to_store_logs']
         pathlib.Path(self.path_to_store_logs).mkdir(parents=True, exist_ok=True)
 
-        self.option_stat = OptionVis(self.env_config['shape_xy'], **config['vis_options'])
-        self.empowerment_vis = EmpowermentVis(self.env_config['shape_xy'], self.agent.empowerment,
-                                              self.environment,
-                                              self.agent.empowerment_horizon,
-                                              self.agent.hierarchy.visual_block.sp)
+        if config['environment_type'] == 'gw':
+            self.option_stat = OptionVis(self.env_config['shape_xy'], **config['vis_options'])
+            self.empowerment_vis = EmpowermentVis(self.env_config['shape_xy'], self.agent.empowerment,
+                                                  self.environment,
+                                                  self.agent.empowerment_horizon,
+                                                  self.agent.hierarchy.visual_block.sp)
+        else:
+            self.option_stat = None
+            self.environment_vis = None
+
         self.option_start_pos = None
         self.option_end_pos = None
         self.last_options_usage = dict()
@@ -187,10 +192,11 @@ class HIMAgentRunner:
 
             obs = self.observation_adapter.adapt(obs)
 
-            self.agent.real_pos = get_unshifted_pos(
-                    self.environment.env.agent.position,
-                    self.environment.env.renderer.shape.top_left_point
-                )
+            if isinstance(self.environment, BioGwLabEnvironment):
+                self.agent.real_pos = get_unshifted_pos(
+                        self.environment.env.agent.position,
+                        self.environment.env.renderer.shape.top_left_point
+                    )
 
             if is_first:
                 self.steps_per_goal += self.steps
@@ -469,21 +475,26 @@ class HIMAgentRunner:
             self.environment.act(self.current_action)
 
             # ///logging///
-            if self.environment.callmethod('is_terminal') and (self.environment.env.items_collected > 0):
-                self.goal_reached = True
-                pos = get_unshifted_pos(
-                    self.environment.env.agent.position,
-                    self.environment.env.renderer.shape.top_left_point
-                )
-                if pos in self.terminal_pos_stat:
-                    self.terminal_pos_stat[pos] += 1
-                else:
-                    self.terminal_pos_stat[pos] = 1
-                self.last_terminal_stat = self.terminal_pos_stat[pos]
+            if log_terminal_stat:
+                if self.environment.callmethod('is_terminal') and (self.environment.env.items_collected > 0):
+                    self.goal_reached = True
+                    pos = get_unshifted_pos(
+                        self.environment.env.agent.position,
+                        self.environment.env.renderer.shape.top_left_point
+                    )
+                    if pos in self.terminal_pos_stat:
+                        self.terminal_pos_stat[pos] += 1
+                    else:
+                        self.terminal_pos_stat[pos] = 1
+                    self.last_terminal_stat = self.terminal_pos_stat[pos]
             # \\\logging\\\
 
         if self.logger is not None:
             self.logger.log({"total_steps": self.steps_total})
+
+        if isinstance(self.environment, PulseEnv):
+            self.environment.shutdown()
+        print('Run finished.')
 
     def convert_to_env_action(self, action_pattern):
         pass
