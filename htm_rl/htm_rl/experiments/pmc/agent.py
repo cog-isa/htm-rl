c0b4cf6bb38cbefdbb427ac35f4f37506c08ccbf
gsys <zhekauran@gmail.com>
2021-12-14 17:59:12 +0300

    fix bugs

--- htm_rl/htm_rl/experiments/pmc/agent.py
+++ htm_rl/htm_rl/experiments/pmc/agent.py
@@ -1,17 +1,27 @@
 from htm_rl.modules.v1 import V1
-from .basal_ganglia import BasalGanglia
-from .pmc import PMCToM1Basic
+from basal_ganglia import BasalGanglia
+from pmc import PMCToM1Basic
+import numpy as np
 
 
 class BasicAgent:
-    def __init__(self, config):
-        self.v1 = V1(**config['v1'])
+    def __init__(self,
+                 camera_resolution,
+                 config):
+        self.v1 = V1(camera_resolution,
+                     config['v1']['complex'],
+                     *config['v1']['simple'])
+        config['bg']['input_size'] = self.v1.output_sdr_size
         self.bg = BasalGanglia(**config['bg'])
         self.pmc = PMCToM1Basic(**config['pmc'])
 
     def make_action(self, obs):
-        stimulus = self.v1.compute(obs)
+        stimulus, _ = self.v1.compute(np.array(obs))
+        stimulus = np.concatenate(stimulus)
         response, probs = self.bg.compute(stimulus, learn=True)
+        self.bg.update_stimulus(stimulus)
+        self.bg.update_response(response)
+
         action = self.pmc.compute(response, probs)
         return action
 
