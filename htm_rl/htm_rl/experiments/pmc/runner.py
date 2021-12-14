b926cf6e32010c790741e9fa60a7b21451c8d257
gsys <zhekauran@gmail.com>
2021-12-14 22:37:42 +0300

    add visualization and layout

--- htm_rl/htm_rl/experiments/pmc/runner.py
+++ htm_rl/htm_rl/experiments/pmc/runner.py
@@ -1,6 +1,9 @@
+import kohonen
+
 from environment import ReachAndGrasp2D
 from agent import BasicAgent
 import pygame
+import numpy as np
 
 
 class Runner:
@@ -16,9 +19,12 @@ class Runner:
             if 'window_size' in config.keys():
                 self.window_size = tuple(config['window_size'])
             else:
-                self.window_size = (500, 500)
+                self.window_size = (600, 400)
         else:
             self.window_size = None
+
+        self.camera_layout = (self.window_size[0] - int(self.window_size[0]/3), self.window_size[1])
+        self.bg_layout = (int(self.window_size[0]/3), self.window_size[1]//2)
 
         self.action_update_period = config['action_update_period']
         self.max_steps = config['max_steps']
@@ -44,6 +50,7 @@ class Runner:
 
     def run_gui(self):
         def pil_image_to_surface(pil_image):
+            pil_image = pil_image.convert('RGB')
             return pygame.image.fromstring(
                 pil_image.tobytes(), pil_image.size, pil_image.mode).convert()
         # initialize the pygame module
@@ -60,17 +67,22 @@ class Runner:
         # controls
         key_actions = (
             (pygame.K_LEFT, "slowdown"),
-            (pygame.K_RIGHT, "speedup"))
+            (pygame.K_RIGHT, "speedup"),
+            (pygame.K_r, "reset"),
+            (pygame.K_s, "show_target"))
         # define a variable to control the main loop
         running = True
 
         # main loop
+        n_steps = 0
         screen_time = 0
         action_time = 0
         screen_update_period = self.environment.time_constant
-        action_update_period = self.action_update_period
+        action_update_period = self.action_update_period // self.environment.time_constant
+        show_goal = False
         while running:
             # event handling, gets all event from the event queue
+            action = None
             for event in pygame.event.get():
                 # only do something if the event is of type QUIT
                 if event.type == pygame.QUIT:
@@ -80,6 +92,15 @@ class Runner:
                     for k, a in key_actions:
                         if event.key == k:
                             action = a
+            if action == 'reset':
+                self.environment.reset()
+                self.agent.reset()
+            elif action == 'speedup':
+                screen_update_period -= 10
+            elif action == 'slowdown':
+                screen_update_period += 10
+            elif action == 'show_target':
+                show_goal = not show_goal
 
             update_screen = False
             update_action = False
@@ -95,16 +116,42 @@ class Runner:
                 action = self.agent.make_action(obs)
                 self.agent.reinforce(reward)
                 self.environment.act(action)
+                n_steps += 1
+
+                im_size = int(round(self.agent.pmc.n_neurons**0.5))
+                probs = np.zeros(im_size**2)
+                probs[self.agent.response] = self.agent.probs
+                heatmap = kohonen.heatmap(probs.reshape((im_size, im_size)))
+                heatmap = heatmap.resize(self.bg_layout)
+                image = pil_image_to_surface(heatmap)
+                screen.blit(image, dest=(0, 0))
+
+                heatmap = self.agent.pmc.pmc.average_neuron_distance_heatmap()
+                heatmap = heatmap.resize(self.bg_layout)
+                image = pil_image_to_surface(heatmap)
+                screen.blit(image, dest=(0, self.bg_layout[1]))
+
+                pygame.display.flip()
             if update_screen:
                 self.environment.simulation_step()
-                image = self.environment.render_rgb(camera_resolution=self.window_size)
+                image = self.environment.render_rgb(
+                    camera_resolution=self.camera_layout,
+                    show_goal=show_goal)
                 image = pil_image_to_surface(image)
-                screen.blit(image, dest=(0, 0))
+                screen.blit(image, dest=(self.bg_layout[0], 0))
                 pygame.display.flip()
+                action_time += 1
+                if self.environment.goal_grabbed:
+                    self.environment.reset()
+                    self.agent.reset()
+
+            if n_steps > self.max_steps:
+                self.environment.reset()
+                self.agent.reset()
+                n_steps = 0
 
             dt = clock.tick()
             screen_time += dt
-            action_time += dt
 
 
 if __name__ == '__main__':
