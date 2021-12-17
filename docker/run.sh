7f51fb016c1fef6f7c2cac57f91be5e6cdddbe0a
Petr Kuderov <pkuderov@gmail.com>
2021-12-17 16:42:34 +0300

    add detached and port forwarding flags to docker run script; add inside-docker jupyter run script

--- docker/run.sh
+++ docker/run.sh
@@ -3,6 +3,8 @@ docker run \
     --platform linux/amd64 \
     --name hima \
     -v $PWD:/app/hima \
+    -p 8888:8888 \
     -it \
+    -d \
     pkuderov/hima \
     zsh
---
+++ docker/run_jupyter_lab.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+jupyter lab \
+    --port=8888 \
+    --ip=0.0.0.0 \
+    --no-browser \
+    --allow-root
