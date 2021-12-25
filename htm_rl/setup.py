2d5e54355f92e243552844b85dd370b8fce43c15
gsys <zhekauran@gmail.com>
2021-12-25 18:48:01 +0300

    delete redundant init file and return authorship to Petr Kuderov:)

--- htm_rl/setup.py
+++ htm_rl/setup.py
@@ -5,8 +5,8 @@ setup(
     version='1.0',
     description='RL agent based on HTM framework',
     url='https://github.com/cog-isa/htm-rl/',
-    author='Evgenij Dzhivelikian',
-    author_email='dzhivelikian@gmail.com',
+    author='Petr Kuderov',
+    author_email='pkuderov@gmail.com',
     license='MIT',
     packages=find_packages(),
 )
