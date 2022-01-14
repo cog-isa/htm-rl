b2dad6334d46f0a3f185098ebbcd257b56c86f94
gsys <zhekauran@gmail.com>
2022-01-14 15:29:08 +0300

    add IK support

--- htm_rl/htm_rl/envs/coppelia/arm.py
+++ htm_rl/htm_rl/envs/coppelia/arm.py
@@ -1,13 +1,4 @@
-from pyrep.robots.robot_component import RobotComponent
-
-
-class Arm(RobotComponent):
-    def __init__(self,
-        count: int, name: str, num_joints: int, base_name: str = None
-    ):
-        """Count is used for when we have multiple copies of arms"""
-        joint_names = ['%s_joint%d' % (name, i + 1) for i in range(num_joints)]
-        super().__init__(count, name, joint_names, base_name)
+from pyrep.robots.arms.arm import Arm
 
 
 class Pulse75(Arm):
