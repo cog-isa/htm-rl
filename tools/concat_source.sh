8f34dfab90fdf445765f5204d73bfb6d48cab329
Petr Kuderov <pkuderov@gmail.com>
2022-02-28 14:34:12 +0300

    reduce the set of source files to concat

--- tools/concat_source.sh
+++ tools/concat_source.sh
@@ -6,66 +6,81 @@ find ./htm_rl -type f -name '*.py' |
     while read fname ; do
         if [[ $fname == *'__init__'* ]]; then
             echo '==' $fname '===>' Excluded!
-        else
+        elif [[ $fname == *'agents/htm/'* ]]; then
             echo $fname
             echo '' >> $destination
             echo '' >> $destination
             echo "========= $fname source file listing =========" >> $destination
             echo '' >> $destination
             cat $fname >> $destination
-        fi
-    done
-
-find ./watcher -type f -name '*.py' | 
-    while read fname ; do
-        if [[ $fname == *'__init__'* ]]; then
+        elif [[ $fname == *'/modules/'* ]]; then
+            echo $fname
+            echo '' >> $destination
+            echo '' >> $destination
+            echo "========= $fname source file listing =========" >> $destination
+            echo '' >> $destination
+            cat $fname >> $destination
+        else
             echo '==' $fname '===>' Excluded!
-        else
-            echo $fname
-            echo '' >> $destination
-            echo '' >> $destination
-            echo "========= $fname source file listing =========" >> $destination
-            echo '' >> $destination
-            cat $fname >> $destination
+            # echo $fname
+            # echo '' >> $destination
+            # echo '' >> $destination
+            # echo "========= $fname source file listing =========" >> $destination
+            # echo '' >> $destination
+            # cat $fname >> $destination
         fi
     done
 
-find ./htm_rl -type f -name '*.yml' | 
-    while read fname ; do
-        echo $fname
-        echo '' >> $destination
-        echo '' >> $destination
-        echo "========= $fname config file listing =========" >> $destination
-        echo '' >> $destination
-        cat $fname >> $destination
-    done
+# find ./watcher -type f -name '*.py' | 
+#     while read fname ; do
+#         if [[ $fname == *'__init__'* ]]; then
+#             echo '==' $fname '===>' Excluded!
+#         else
+#             echo $fname
+#             echo '' >> $destination
+#             echo '' >> $destination
+#             echo "========= $fname source file listing =========" >> $destination
+#             echo '' >> $destination
+#             cat $fname >> $destination
+#         fi
+#     done
 
-find ./htm_rl -type f -name '*.yaml' | 
-    while read fname ; do
-        echo $fname
-        echo '' >> $destination
-        echo '' >> $destination
-        echo "========= $fname config file listing =========" >> $destination
-        echo '' >> $destination
-        cat $fname >> $destination
-    done
+# find ./htm_rl -type f -name '*.yml' | 
+#     while read fname ; do
+#         echo $fname
+#         echo '' >> $destination
+#         echo '' >> $destination
+#         echo "========= $fname config file listing =========" >> $destination
+#         echo '' >> $destination
+#         cat $fname >> $destination
+#     done
 
-find ./htm_rl -type f -name '*.json' | 
-    while read fname ; do
-        echo $fname
-        echo '' >> $destination
-        echo '' >> $destination
-        echo "========= $fname config file listing =========" >> $destination
-        echo '' >> $destination
-        cat $fname >> $destination
-    done
+# find ./htm_rl -type f -name '*.yaml' | 
+#     while read fname ; do
+#         echo $fname
+#         echo '' >> $destination
+#         echo '' >> $destination
+#         echo "========= $fname config file listing =========" >> $destination
+#         echo '' >> $destination
+#         cat $fname >> $destination
+#     done
 
-find ./htm_rl -type f -name '*.map' | 
-    while read fname ; do
-        echo $fname
-        echo '' >> $destination
-        echo '' >> $destination
-        echo "========= $fname environment map file listing =========" >> $destination
-        echo '' >> $destination
-        cat $fname >> $destination
-    done
+# find ./htm_rl -type f -name '*.json' | 
+#     while read fname ; do
+#         echo $fname
+#         echo '' >> $destination
+#         echo '' >> $destination
+#         echo "========= $fname config file listing =========" >> $destination
+#         echo '' >> $destination
+#         cat $fname >> $destination
+#     done
+
+# find ./htm_rl -type f -name '*.map' | 
+#     while read fname ; do
+#         echo $fname
+#         echo '' >> $destination
+#         echo '' >> $destination
+#         echo "========= $fname environment map file listing =========" >> $destination
+#         echo '' >> $destination
+#         cat $fname >> $destination
+#     done
